aded15f03d2ebb8742c6af78883130fc07bfafdf
Børre Gaup <albbas@gmail.com>
2018-08-28 22:39:33 +0200

    Not all data received from eXist is json
    Need to turn it into json before parsing it

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -3,10 +3,11 @@ import PropTypes from 'prop-types';
 
 export default class Posts extends Component {
   render () {
-    const isjson = JSON.parse(this.props.posts);
+    let data = this.props.posts
+    const isjson = (data.indexOf('{') == 0) ? '[' + data.slice(data.indexOf('{')+1,data.lastIndexOf('}')) + ']' : data
     return (
       <ul>
-        {isjson.map((post, i) => <li key={i}>{post.term}</li>)}
+        {JSON.parse(isjson).map((post, i) => <li key={i}>{post.term}</li>)}
       </ul>
     );
   }
