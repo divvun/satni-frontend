35fdf401e711e9b2d9351e5aa0f2d64f270086af
Børre Gaup <albbas@gmail.com>
2018-08-28 14:37:18 +0200

    The reddit example from redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import 'babel-polyfill';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import React from 'react';
+import { render } from 'react-dom';
+import Root from './containers/Root';
+
+render(
+  <Root />,
+  document.getElementById('root')
+);
