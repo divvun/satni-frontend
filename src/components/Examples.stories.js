0a87788fd7165df0b96fdf5b3a6d72dbfb546791
Børre Gaup <albbas@gmail.com>
2018-09-20 13:46:40 +0200

    Examples as a story

---
+++ src/components/Examples.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+import Examples from './Examples';
+
+const examples = [
+  { x: 'original0', xt: 'translation0', key: 0 },
+  { x: 'original1', xt: 'translation1', key: 0 }
+];
+
+storiesOf('Examples', module)
+  .add('default', () => <Examples examples={examples} />);
