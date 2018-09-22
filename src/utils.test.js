8838176be463d01955ba92d6c4f7dd333105831f
Børre Gaup <albbas@gmail.com>
2018-09-22 17:06:01 +0200

    Move testdata to a separate file
    Storybook insisted that the function describe was not defined when
    importing data from utils.test.js

--- src/utils.test.js
+++ src/utils.test.js
@@ -8,114 +8,12 @@ import {
   normaliseSDTerm,
   normaliseParadigm
 } from './utils';
-
-export const resultDictWithExamples = {
-  'translations': [
-    {
-      'pos': 'N',
-      'lang': 'sme',
-      'lemma': 'oainnáhus'
-    },
-    {
-      'pos': 'N',
-      'lang': 'nob',
-      'lemma': 'syn'
-    }
-  ],
-  'examples': [
-    {
-      'x': 'Iskkan sihkkut váivves oainnáhusa iežan čalmmiin, muhto dat liikká čuovvu mu.',
-      'xt': 'Jeg prøver å vaske vekk det triste synet fra øynene mine, men det følger med likevel.'
-    },
-    {
-      'x': 'Lean áidna vilges olmmoš ja dieđusge oainnáhus.',
-      'xt': 'Jeg er det eneste hvite mennesket og selvfølgelig et syn.'
-    }
-  ],
-  termwikiref: '-1',
-  dict: 'smenob'
-};
-
-export const resultDictWithoutExamples = {
-  'dict': 'smefin',
-  'examples': [],
-  'termwikiref': '-1',
-  'translations': [
-    {
-      'pos': 'N',
-      'lang': 'sme',
-      'lemma': 'guolladat'
-    },
-    {
-      'pos': 'N',
-      'lang': 'fin',
-      'lemma': 'kalvoin verkonkudonnassa'
-    }
-  ]
-};
-
-export const resultSDTerm = {
-  stems: [
-    {
-      'lemma': 'gođđinmuorra',
-      'pos': 'S',
-      'lang': 'sme'
-    },
-    {
-      'lemma': 'guolládat',
-      'pos': 'S',
-      'lang': 'sme'
-    },
-    {
-      'lemma': 'guolla',
-      'pos': 'S',
-      'lang': 'sme'
-    },
-    {
-      'lemma': 'målepinne',
-      'pos': 'S',
-      'lang': 'nob'
-    }
-  ],
-  dict: 'SD-terms'
-};
-
-export const resultTermWiki = {
-  stems: [
-    {
-      'lemma': 'androgyn',
-      'lang': 'smn',
-      'pos': 'N'
-    },
-    {
-      'lemma': 'androgyne',
-      'lang': 'eng',
-      'pos': 'N'
-    },
-    {
-      'lemma': 'androgynlâšvuotâ',
-      'lang': 'smn',
-      'pos': 'A'
-    },
-    {
-      'lemma': 'androgynvuotâ',
-      'lang': 'smn',
-      'pos': undefined
-    },
-    {
-      'lemma': 'androgyna',
-      'lang': 'sme',
-      'pos': 'N'
-    },
-    {
-      'lemma': 'androgyyni',
-      'lang': 'fin',
-      'pos': 'N'
-    }
-  ],
-  termwikiref: 'Servodatdieđa:androgyna',
-  dict: 'termwiki'
-};
+import {
+  resultSDTerm,
+  resultDictWithExamples,
+  resultDictWithoutExamples,
+  resultTermWiki
+} from './utils_testdata';
 
 describe('Massage data from eXist', () => {
   it('Turn text in to JSON', () => {
