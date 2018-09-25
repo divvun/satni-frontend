7811cec90033610696c01f3fb4998aa31bc6026a
Børre Gaup <albbas@gmail.com>
2018-09-25 18:58:15 +0200

    Use Material-UI for tables

--- src/components/NounParadigm.js
+++ src/components/NounParadigm.js
@@ -1,48 +1,52 @@
 import React from 'react';
-import { css } from 'react-emotion';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
 const CaseRow = ({wcase, value}) => {
   if (value instanceof Array) {
     return (
-      <tr>
-        <td>{wcase.replace('_both', '')}</td>
-        <td className={css({colSpan: '2'})}>{value.map((word, i) => {
+      <TableRow>
+        <TableCell>{wcase.replace('_both', '')}</TableCell>
+        <TableCell colSpan={2}>{value.map((word, i) => {
           return (
             <div key={i}>{word}</div>
           );
-        })}</td>
-      </tr>
+        })}</TableCell>
+      </TableRow>
     );
   }
 
   return (
-    <tr>
-      <td>{wcase}</td>
+    <TableRow>
+      <TableCell>{wcase}</TableCell>
       {Object.keys(value).map((key, j) => {
         return (
-          <td key={j}>{value[key].map((word, i) => {
+          <TableCell key={j}>{value[key].map((word, i) => {
             return (
               <div key={i}>{word}</div>
             );
-          })}</td>
+          })}</TableCell>
         );
       })}
-    </tr>
+    </TableRow>
   );
 };
 
 const NounParadigm = ({paradigm}) => {
   return (
-    <table>
-      <thead>
-        <th>&nbsp;</th><th>Sg</th><th>Pl</th>
-      </thead>
-      <tbody>
+    <Table>
+      <TableHead>
+        <TableCell>&nbsp;</TableCell><TableCell>Sg</TableCell><TableCell>Pl</TableCell>
+      </TableHead>
+      <TableBody>
         {Object.keys(paradigm).map((p, i) => {
           return <CaseRow key={p} wcase={p} value={paradigm[p]} />;
         })}
-      </tbody>
-    </table>
+      </TableBody>
+    </Table>
   );
 };
 
