74d42ee2666283ff83b4e595baa7330642b8c262
Børre Gaup <albbas@gmail.com>
2018-09-27 20:19:21 +0200

    Add a version of the AdjParadigm that is generated from a normalised input

--- src/components/AdjParadigm.stories.js
+++ src/components/AdjParadigm.stories.js
@@ -4,11 +4,10 @@ import { action } from '@storybook/addon-actions';
 
 import AdjParadigm from './AdjParadigm';
 import {
-  resultSmaNounParadigm,
-  resultSmeNounParadigm,
-  resultSmjNounParadigm,
-  resultSmnNounParadigm
+  resultSmeAdjParadigm,
+  resultSmjAdjParadigm
 } from '../utils_testdata';
 
 storiesOf('AdjParadigm', module)
-  .add('North saami adjective', () => <AdjParadigm paradigm={resultSmeNounParadigm} />);
+  .add('North saami adjective', () => <AdjParadigm paradigm={resultSmeAdjParadigm} />)
+  .add('Lule saami adjective', () => <AdjParadigm paradigm={resultSmjAdjParadigm} />);
