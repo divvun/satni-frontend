5b87fd2fbb9a31d9bd7f5c06d5a3c91f3c7a2f9b
Børre Gaup <albbas@gmail.com>
2018-10-19 01:44:26 +0200

    Handle JustermTana articles in the presentation

--- src/components/PresentArticles.stories.js
+++ src/components/PresentArticles.stories.js
@@ -7,17 +7,37 @@ import {
   resultSDTerm,
   resultDictWithExamples,
   resultDictWithoutExamples,
-  resultTermWiki
+  resultTermWiki,
+  resultJustermTana
 } from '../utils_testdata';
 
 const isFetching = true;
 const articles = [];
 
 storiesOf('Articles', module)
-  .add('Loading', () => <Articles isFetching={isFetching} articles={articles} />)
-  .add('DictArticle with examples', () => <Articles isFetching={!isFetching} articles={[resultDictWithExamples]} />)
-  .add('DictArticle without examples', () => <Articles isFetching={!isFetching} articles={[resultDictWithoutExamples]} />)
-  .add('Only TermWikiArticle', () => <Articles isFetching={!isFetching} articles={[resultTermWiki]} />)
-  .add('Only SDTermArticle', () => <Articles isFetching={!isFetching} articles={[resultSDTerm]} />)
+  .add('Loading', () => <Articles
+    isFetching={isFetching}
+    articles={articles} />)
+  .add('DictArticle with examples', () => <Articles
+    isFetching={!isFetching}
+    articles={[resultDictWithExamples]} />)
+  .add('DictArticle without examples', () => <Articles
+    isFetching={!isFetching}
+    articles={[resultDictWithoutExamples]} />)
+  .add('Only TermWikiArticle', () => <Articles
+    isFetching={!isFetching}
+    articles={[resultTermWiki]} />)
+  .add('Only SDTermArticle', () => <Articles
+    isFetching={!isFetching}
+    articles={[resultSDTerm]} />)
+  .add('Only JustermTana', () => <Articles
+    isFetching={!isFetching}
+    articles={[resultJustermTana]} />)
   .add('All types', () => <Articles isFetching={!isFetching}
-    articles={[resultDictWithExamples, resultDictWithoutExamples, resultTermWiki, resultSDTerm]} />);
+    articles={[
+      resultDictWithExamples,
+      resultDictWithoutExamples,
+      resultTermWiki,
+      resultSDTerm,
+      resultJustermTana
+    ]} />);
