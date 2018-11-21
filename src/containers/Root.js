7f6b80fd0317306b93712462feb94b3a6bde8496
Børre Gaup <albbas@gmail.com>
2018-11-21 15:03:50 +0100

    Catch the redirect done by the Searcher

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AsyncApp from './AsyncApp';
 import ErrorBoundary from '../components/ErrorBoundary';
 
+const NoMatch = () => <Redirect to='/' />;
+
 const Root = ({ store }) => (
   <Provider store={store}>
     <MuiThemeProvider>
       <ErrorBoundary>
-        <AsyncApp />
+        <Router>
+          <Switch>
+            <Route path='/' exact component={AsyncApp} />
+            <Route path='/article/:lemma' component={AsyncApp} />
+            <Route path='*' component={NoMatch} />
+          </Switch>
+        </Router>
       </ErrorBoundary>
     </MuiThemeProvider>
   </Provider>
