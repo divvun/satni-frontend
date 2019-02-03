60ddff77f477162b592f210d90e234436a33135a
Børre Gaup <albbas@gmail.com>
2019-02-03 02:14:31 +0100

    Add support for restring in Stem

--- src/components/Stem.stories.js
+++ src/components/Stem.stories.js
@@ -8,6 +8,7 @@ const stem = { lemma: 'guolli', pos: 'N', lang: 'sme', key: 0};
 
 storiesOf('Stem', module)
   .add('default', () => <Stem stem={stem} />)
+  .add('with_re', () => <Stem stem={{...stem, re: '(restring)'}} />)
   .add('key_nonzero', () => <Stem stem={{...stem, key: 1}} />)
   .add('lang_nonsme', () => <Stem stem={{...stem, lang: 'smj'}} />)
   .add('non_sami', () => <Stem stem={{...stem, lang: 'nob'}} />);
