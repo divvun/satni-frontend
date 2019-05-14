a37f4eb31d0ca2b3d6e523b70a58b5f70e75fc87
Børre Gaup <albbas@gmail.com>
2019-05-14 10:50:52 +0200

    Add a feedback region in the footer
    Layout and such borrowed from
    https://github.com/mui-org/material-ui/blob/master/docs/src/pages/getting-started/page-layout-examples/pricing/Pricing.js

--- src/containers/AsyncApp.js
+++ src/containers/AsyncApp.js
@@ -1,66 +1,125 @@
 import React from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+
 import styled, { css } from 'react-emotion';
 import Searcher from '../components/Searcher';
 import Articles from '../components/Articles';
 
-const MyGrid = styled('div')({
-  margin: 'auto',
-  // You can use @supports with glamor!
-  // So you can use @supports with glamorous as well!
-  '@supports (display: grid)': {
-    display: 'grid',
-    gridGap: 10,
-    gridTemplateAreas: `
-      "header"
-      "content"
-      "footer"
-    `
+const styles = theme => ({
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.common.white
+    }
+  },
+  appBar: {
+    position: 'relative'
+  },
+  toolbarTitle: {
+    flex: 1
+  },
+  layout: {
+    width: 'auto',
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(900 + theme.spacing.unit * 3 * 2)]: {
+      width: 900,
+      marginLeft: 'auto',
+      marginRight: 'auto'
+    }
+  },
+  heroContent: {
+    maxWidth: 600,
+    margin: '0 auto',
+    padding: `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 6}px`
+  },
+  footer: {
+    marginTop: theme.spacing.unit * 8,
+    borderTop: `1px solid ${theme.palette.divider}`,
+    padding: `${theme.spacing.unit * 6}px 0`
   }
 });
 
-const Box = styled('div')({
-  padding: 10,
-  fontSize: '120%'
-});
+const footers = [
+  {
+    title: 'Feedback',
+    description: [
+      {
+        text: 'Please report any errors to us',
+        address: 'mailto:feedback@divvun.no'
+      }
+    ]
+  },
+  {
+    title: 'Sámi dictionaries and terms delivered by',
+    description: [
+      {
+        text: 'Divvun',
+        address: 'http://divvun.no'
+      },
+      {
+        text: 'Giellagáldu',
+        address: 'https://giella.org'
+      },
+      {
+        text: 'Giellatekno',
+        address: 'http://giellatekno.uit.no'
+      }
+    ]
+  }
+];
 
-const AsyncApp = ({match}) => (
-  <MyGrid>
-    <Box css={{ gridArea: 'header'}}>
-      <div className={css({
-        fontWeight: 'bold',
-        fontSize: '200%',
-        textAlign: 'center'
-      })}>
-            sátni.org
-          </div>
-    </Box>
-    <Box css={{
-      gridArea: 'content',
-      paddingBottom: '50px'
-    }}>
-      <Searcher />
-      {match.params.lemma ?
-        <Articles lemma={match.params.lemma} /> :
+const AsyncApp = ({classes, match}) => (
+  <React.Fragment>
+    <CssBaseline />
+    <AppBar position='static' color='default' className={classes.appBar}>
+      <Toolbar>
+        <Typography variant='h6' color='inherit' noWrap className={classes.toolbarTitle}>
+        sátni.org
+      </Typography>
+      </Toolbar>
+    </AppBar>
+    <main className={classes.layout}>
+      <div className={classes.heroContent}>
+        <Searcher />
+        {match.params.lemma ?
+          <Articles lemma={match.params.lemma} /> :
         null
       }
-    </Box>
-    <Box css={{
-      gridArea: 'footer'
-    }}>
-      <footer className={css({
-        position: 'fixed',
-        left: '0px',
-        bottom: '0px',
-        height: '50px',
-        width: '100%',
-        background: '#999',
-        textAlign: 'center'
-      })}>
-          Sámi dictionaries and terms delivered by<br />
-        <a href='http://divvun.no'>Divvun</a>, <a href='http://giella.org'>Giellagáldu</a> and <a href='http://giellatekno.uit.no'>Giellatekno</a>
-      </footer>
-    </Box>
-  </MyGrid>
+      </div>
+    </main>
+    {/* Footer */}
+    <footer className={classNames(classes.footer, classes.layout)}>
+      <Grid container spacing={32} justify='space-evenly'>
+        {footers.map(footer => (
+          <Grid item xs key={footer.title}>
+            <Typography variant='h6' color='textPrimary' gutterBottom>
+              {footer.title}
+            </Typography>
+            {footer.description.map(item => (
+              <Typography key={item.text} variant='subtitle1' color='textSecondary'>
+                <a href={item.address}>{item.text}</a>
+              </Typography>
+              ))}
+          </Grid>
+          ))}
+      </Grid>
+    </footer>
+    {/* End footer */}
+
+  </React.Fragment>
     );
 
-export default AsyncApp;
+AsyncApp.propTypes = {
+  classes: PropTypes.object.isRequired,
+  matches: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(AsyncApp);
