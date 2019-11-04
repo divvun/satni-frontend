eeb4a8970e88bf454071d18ecd05797424ba70d7
Børre Gaup <albbas@gmail.com>
2019-11-04 20:35:00 +0100

    Parse queryparameters in the LemmaDetails component

--- src/components/LemmaDetails.js
+++ src/components/LemmaDetails.js
@@ -1,12 +1,16 @@
 import React from 'react';
-
+import qs from 'qs';
 import Korp from './Korp';
+import Paradigm from './Paradigm';
 
-const LemmaDetails = ({lemma, language, pos}) => (
-  <div>
-    <span>{lemma} {pos} {language}</span>
-    <Korp lemma={lemma} language={language} />
-  </div>
-);
+const LemmaDetails = ({match, location}) => {
+  const location_dict = qs.parse(location.search.slice(1));
+  return (
+    <div>
+      <Paradigm lemma={location_dict.lemma} language={location_dict.lang} pos={location_dict.pos} />
+      <Korp lemma={location_dict.lemma} language={location_dict.lang} />
+    </div>
+  );
+};
 
 export default LemmaDetails;
