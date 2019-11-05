5c6b9db8266b6f69de09fca0b3198c33e18a8fc6
Børre Gaup <albbas@gmail.com>
2019-11-05 20:36:02 +0100

    Make details link

--- src/components/TermStem.js
+++ src/components/TermStem.js
@@ -1,31 +1,10 @@
 import React from 'react';
-import { css } from 'react-emotion';
-import { Set } from 'immutable';
+import { Link } from 'react-router-dom';
 
-const korpLangs = Set.of('sma', 'sme', 'smj', 'smn', 'sms');
-
-const KorpLink = (lang, lemma) => {
-  const searchString = `Search for ${lemma} in Korp`;
-  const korpAddress = (lang !== 'sme' && korpLangs.has(lang))
-                        ? `http://gtweb.uit.no/korp/?mode=${lang}#?search=cqp|[lemma%3D"${lemma}"]`
-                        : `http://gtweb.uit.no/korp/#?search=cqp|[lemma%3D"${lemma}"]`;
+const TermStem = ({ stem: {lemma, pos, lang, key}}) => {
   return (
-    <span className={css({
-      float: 'right',
-      display: 'inline'
-    })}><a href={korpAddress} target='_blank' rel='noopener noreferrer'>{searchString}</a></span>
-  );
-};
-
-const TermStem = ({ stem: {lemma, pos, lang, key, re}}) => {
-  return (
-    <div
-      className={css({
-        width: '100%',
-        key: key
-      })}>
-      <span className={css({ display: 'inline'})}>{lemma} {re} ({pos})</span>
-      {korpLangs.has(lang) && (KorpLink(lang, lemma))}
+    <div>
+      <Link to={`/details?lemma=${lemma}&lang=${lang}&pos=${pos}`}>{lemma} ({pos})</Link>
     </div>
   );
 };
