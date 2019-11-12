a3a419c423ca69b7be611082bdd55ab3c83b57af
Børre Gaup <albbas@gmail.com>
2019-11-12 21:16:20 +0100

    Do not use Link here

--- src/components/TermStem.js
+++ src/components/TermStem.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const TermStem = ({ stem: {lemma, pos, lang, key}}) => {
   return (
     <div>
-      <Link to={`/details?lemma=${lemma}&lang=${lang}&pos=${pos}`}>{lemma} ({pos})</Link>
+      <a href={`/details?lemma=${lemma}&lang=${lang}&pos=${pos}`}>{lemma} ({pos})</a>
     </div>
   );
 };
