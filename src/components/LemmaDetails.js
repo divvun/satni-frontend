dde4d997647e95eae069f821d9bb419d00f58eb3
Børre Gaup <albbas@gmail.com>
2019-11-12 21:38:03 +0100

    KorpLink, then Paradigm

--- src/components/LemmaDetails.js
+++ src/components/LemmaDetails.js
@@ -7,8 +7,8 @@ const LemmaDetails = ({match, location}) => {
   const location_dict = qs.parse(location.search.slice(1));
   return (
     <div>
-      <Paradigm lemma={location_dict.lemma} language={location_dict.lang} pos={location_dict.pos} />
       <Korp lemma={location_dict.lemma} language={location_dict.lang} />
+      <Paradigm lemma={location_dict.lemma} language={location_dict.lang} pos={location_dict.pos} />
     </div>
   );
 };
