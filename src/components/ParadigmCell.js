3b3f0dddbc34dfd27816203f4b25398084e38e39
Børre Gaup <albbas@gmail.com>
2019-11-12 18:43:48 +0100

    Pick up the parameters the correct way

--- src/components/ParadigmCell.js
+++ src/components/ParadigmCell.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import TableCell from '@material-ui/core/TableCell';
 
-const ParadigmCell = ({key}, {values}) => (
+const ParadigmCell = ({key, values}) => (
   <TableCell key={key}>{values.map(value => <div>{value}</div>)}</TableCell>
 );
 
