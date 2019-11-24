2357c8bcdd533607ab28474a8e4090ddee9f5e9f
Børre Gaup <albbas@gmail.com>
2019-11-24 20:53:31 +0100

    Move the router into AsyncApp, as this is where the action is

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AsyncApp from './AsyncApp';
 import ErrorBoundary from '../components/ErrorBoundary';
-import LemmaDetails from '../components/LemmaDetails.js';
-
-const NoMatch = () => <Redirect to='/' />;
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <MuiThemeProvider>
       <ErrorBoundary>
-        <Router>
-          <Switch>
-            <Route path='/' exact component={AsyncApp} />
-            <Route path='/article/:lemma' component={AsyncApp} />
-            <Route path='/details' component={LemmaDetails} />
-          </Switch>
-        </Router>
+      <AsyncApp/>
       </ErrorBoundary>
     </MuiThemeProvider>
   </Provider>
