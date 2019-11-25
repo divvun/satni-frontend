c6e8ffaca468e93576e2f196663ea7408f05e047
Børre Gaup <albbas@gmail.com>
2019-11-25 04:02:14 +0100

    Paint a pretty blue line around the details

--- src/components/LemmaDetails.js
+++ src/components/LemmaDetails.js
@@ -2,14 +2,15 @@ import React from 'react';
 import qs from 'qs';
 import Korp from './Korp';
 import Paradigm from './Paradigm';
+import { ArticleDiv } from '../components';
 
 const LemmaDetails = ({match, location}) => {
   const location_dict = qs.parse(location.search.slice(1));
   return (
-    <div>
+    <ArticleDiv>
       <Korp lemma={location_dict.lemma} language={location_dict.lang} />
       <Paradigm lemma={location_dict.lemma} language={location_dict.lang} pos={location_dict.pos} />
-    </div>
+    </ArticleDiv>
   );
 };
 
