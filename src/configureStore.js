63ba076234835192c39e86fe1d21d91f005571c2
Børre Gaup <albbas@gmail.com>
2019-12-02 17:41:44 +0100

    Introduce redux toolkit in the store creation

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,22 +1,8 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
+import { configureStore } from "@reduxjs/toolkit"
 import rootReducer from './reducers';
 
-const loggerMiddleware = createLogger();
+const satniStore = configureStore({
+  reducer: rootReducer
+})
 
-const configureStore = (preloadedState) => {
-  const middlewares = [thunkMiddleware];
-
-  if (process.env.NODE_ENV !== 'production') {
-    middlewares.push(loggerMiddleware);
-  }
-
-  return createStore(
-    rootReducer,
-    preloadedState,
-    applyMiddleware(...middlewares)
-  );
-};
-
-export default configureStore;
+export default satniStore;
