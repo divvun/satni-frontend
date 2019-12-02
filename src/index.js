9027b14137f26cdf1ba795b86a3592868219e86f
Børre Gaup <albbas@gmail.com>
2019-12-02 17:41:44 +0100

    Introduce redux toolkit in the store creation

--- src/index.js
+++ src/index.js
@@ -4,15 +4,13 @@ import * as Sentry from '@sentry/browser';
 import React from 'react';
 import { render } from 'react-dom';
 import Root from './containers/Root';
-import configureStore from './configureStore';
-
-const store = configureStore();
+import satniStore from './configureStore';
 
 Sentry.init({
   dsn: 'https://65960ebe11524e22b83637dfe43344a2@sentry.io/1303235'
 });
 
 render(
-  <Root store={store} />,
+  <Root store={satniStore} />,
   document.getElementById('root')
 );
