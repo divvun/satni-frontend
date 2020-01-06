ebc294be29f69826634ae68b80d7fcc27e851d9d
Børre Gaup <albbas@gmail.com>
2020-01-06 13:23:16 +0100

    Add a async/await version of the search item fetcher

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,25 @@
 import fetch from 'cross-fetch';
 
-export const fetchParadigm = async (stem) => {
+export const fetchParadigm = async(stem) => {
   const url = `http://gtweb.uit.no/cgi-bin/smi/smi.cgi?json=true&text=${stem.lemma}&pos=${stem.pos}&mode=standard&action=paradigm&lang=${stem.language}`;
   const response = await fetch(encodeURI(url), {credentials: 'same-origin', mode: 'no-cors'});
   const data = await response.json();
   return data;
 };
 
-export const getArticles = async (lemma) => {
+export const getArticles = async(lemma) => {
   const url = `https://satni.uit.no/satnibackend/article/${lemma}`;
   const response = await fetch(encodeURI(url));
   const text = await response.text();
 
   return text;
 };
+
+export const getSearchItems = async(key) => {
+  const url = `https://satni.uit.no/satnibackend/search?query=${key}`;
+  const response = await fetch(encodeURI(url));
+  const text = await response.text();
+
+  return text;
+};
+
