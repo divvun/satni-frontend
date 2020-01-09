371b9955362121db1267eeb3574c78120d3cbdb8
Børre Gaup <albbas@gmail.com>
2020-01-09 07:18:51 +0100

    Fix paths

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import 'babel-polyfill';
 import * as Sentry from '@sentry/browser';
 import React from 'react';
 import { render } from 'react-dom';
-import Root from './containers/Root';
-import satniStore from './configureStore';
+import Root from 'containers/Root';
+import satniStore from 'configureStore';
 
 Sentry.init({
   dsn: 'https://65960ebe11524e22b83637dfe43344a2@sentry.io/1303235'
