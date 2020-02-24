8f463f91d3e1b13bb1df13ed8604a4ac4910c19d
Børre Gaup <albbas@gmail.com>
2020-02-24 14:00:02 +0100

    Move terms into MultiLingualConcept

--- backend/terms/models.py
+++ backend/terms/models.py
@@ -3,11 +3,11 @@ from lemmas.models import Lemma
 
 class MultiLingualConcept(models.Model):
     name = models.TextField()
+    terms = models.ManyToManyField(Lemma)
 
 
 class Concept(models.Model):
     language = models.TextField()
     definition = models.TextField(blank=True)
     explanation = models.TextField(blank=True)
-    terms = models.ManyToManyField(Lemma)
     mconcept = models.ForeignKey(MultiLingualConcept, on_delete=models.CASCADE)
