fcc21b03251b3012ba3fb9d4ae0cafea41a7660e
Børre Gaup <albbas@gmail.com>
2020-02-27 15:41:30 +0100

    The needed queries and such to do the sátni.org searches

--- backend/lemmas/models.py
+++ backend/lemmas/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-
 class Lemma(models.Model):
     lemma = models.TextField()
     partOfSpeech = models.TextField()
     language = models.TextField()
+    concept = models.ManyToManyField('terms.Concept', related_name='terms', null=True)
