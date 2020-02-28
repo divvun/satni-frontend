f58b62dcf7725e735f256e2654d59bcf6b5cfe0a
Børre Gaup <albbas@gmail.com>
2020-02-28 12:49:48 +0100

    Switch from sqlite to postgresql

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'term1',
+        'USER': 'termuser',
+        'PASSWORD': 'term1',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
