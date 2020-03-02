1715d94ed31a727e2de969561681702f702fc739
Børre Gaup <albbas@gmail.com>
2020-03-02 14:54:46 +0100

    Add the apollo client provider

--- frontend/src/containers/Root.js
+++ frontend/src/containers/Root.js
@@ -2,14 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
 import AsyncApp from './AsyncApp';
 import ErrorBoundary from '../components/ErrorBoundary';
+
+const client = new ApolloClient({
+  uri: 'http://127.0.0.1:8000/graphql/'
+});
+
+// client
+//   .query({
+//     query: gql`
+//       {
+//         lemmas(search: "lim") {
+//           lemma
+//         }
+//       }
+//     `
+//   })
+//   .then(result => console.log(result));
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <MuiThemeProvider>
       <ErrorBoundary>
-      <AsyncApp/>
+        <ApolloProvider client={client}>
+          <AsyncApp />
+        </ApolloProvider>
       </ErrorBoundary>
     </MuiThemeProvider>
   </Provider>
