e23e9e927319daa947ff1417415bc2df01375fe9
Børre Gaup <albbas@gmail.com>
2020-03-21 17:19:31 +0100

    Add keys

--- frontend/src/features/article/LemmaGroups.js
+++ frontend/src/features/article/LemmaGroups.js
@@ -6,14 +6,14 @@ import Stem from './Stem';
 const LemmaGroups = ({ from, to }) => (
   <Grid container spacing={1}>
     <Grid item xs={6}>
-      {from.map(stem =>
-        <Stem stem={stem} />)
+      {from.map((stem, index) =>
+        <Stem key={index} stem={stem} />)
       }
     </Grid>
     <Grid item xs={6}>
-      {to.map(stem =>
-        <Stem stem={stem} />)
-        }
+      {to.map((stem, index) =>
+        <Stem key={index} stem={stem} />)
+      }
     </Grid>
   </Grid>
 );
