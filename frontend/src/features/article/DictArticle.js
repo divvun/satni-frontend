072c1d617f8f008516126906fb2667116cdedab9
Børre Gaup <albbas@gmail.com>
2020-03-21 17:05:32 +0100

    Fix warnings

--- frontend/src/features/article/DictArticle.js
+++ frontend/src/features/article/DictArticle.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
 
 import Examples from './Examples';
 import LemmaGroups from './LemmaGroups';
