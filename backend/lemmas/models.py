9c43bcc4a96b24beed0123a77474552e33d9fc77
Børre Gaup <albbas@gmail.com>
2020-03-23 14:39:10 +0100

    Use the common abbreviation for part of speech

--- backend/lemmas/models.py
+++ backend/lemmas/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Lemma(models.Model):
     lemma = models.TextField()
-    partOfSpeech = models.TextField()
+    pos = models.TextField()
     language = models.TextField()
     concept = models.ManyToManyField('terms.Concept', related_name='terms')
