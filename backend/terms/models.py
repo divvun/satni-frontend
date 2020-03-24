4995ea5a6de558fd5432d2c6cee1eb315709da19
Børre Gaup <albbas@gmail.com>
2020-03-24 02:34:17 +0100

    New models

--- backend/terms/models.py
+++ backend/terms/models.py
@@ -2,13 +2,20 @@ from djongo import models
 
 
 class MultiLingualConcept(models.Model):
-    name = models.TextField(unique=True)
-    lemma = models.ManyToManyField('lemmas.Lemma', related_name='lemmaconcepts')
+    name = models.TextField()
 
+
+class Term(models.Model):
+    status = models.TextField(blank=True)
+    sanctioned = models.BooleanField(default=False)
+    note = models.TextField(blank=True)
+    source = models.TextField(blank=True)
 
 class Concept(models.Model):
     language = models.TextField()
     definition = models.TextField(blank=True)
     explanation = models.TextField(blank=True)
-    mconcept = models.ForeignKey(
-        MultiLingualConcept, on_delete=models.CASCADE, blank=True, null=True)
+    turms = models.ForeignKey(
+        Term, on_delete=models.CASCADE)
+
+
