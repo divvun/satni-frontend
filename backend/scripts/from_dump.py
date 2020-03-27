0ad145223e447cc538b4f611878f255f110af1c9
Børre Gaup <albbas@gmail.com>
2020-03-27 16:23:34 +0100

    More improvements

--- backend/scripts/from_dump.py
+++ backend/scripts/from_dump.py
@@ -20,16 +20,36 @@ langs = {
 
 
 def make_concept(lang, concept_infos, m):
-    for concept_info in concept_infos:
-        if lang == concept_info['language']:
+    def make_c():
+        for concept_info in concept_infos:
+            if lang == concept_info['language']:
+                return Concept.objects.create(
+                    language=langs[lang],
+                    definition=concept_info.get('definition'),
+                    explanation=concept_info.get('explanation'),
+                    multilingualconcepts=m)
+        else:
             return Concept.objects.create(
-                language=langs[lang],
-                definition=concept_info.get('definition'),
-                explanation=concept_info.get('explanation'),
-                multilingualconcepts=m)
-    else:
-        return Concept.objects.create(
-            language=langs[lang], multilingualconcepts=m)
+                language=langs[lang], multilingualconcepts=m)
+
+
+    c = make_c()
+    for expression in same_lang_expressions(
+            lang, concept.related_expressions):
+        l, _ = Lemma.objects.get_or_create(
+            lemma=expression['expression'],
+            pos=expression['pos'],
+            language=langs[lang])
+        term = Term.objects.create(
+            status=expression.get('status'),
+            sanctioned=expression.get('sanctioned', False),
+            note=expression.get('note'),
+            source=expression.get('source'),
+            concept=c,
+            expression=l)
+        l.multilingualconcepts.add(m)
+
+    return c
 
 
 def same_lang_expressions(lang, expressions):
@@ -53,20 +73,6 @@ def make_m():
 def make_c(m, concept):
     for lang in concept.languages():
         c = make_concept(lang, concept.data['concept_infos'], m)
-        for expression in same_lang_expressions(
-                lang, concept.related_expressions):
-            l, _ = Lemma.objects.get_or_create(
-                lemma=expression['expression'],
-                pos=expression['pos'],
-                language=langs[lang])
-            term = Term.objects.create(
-                status=expression.get('status'),
-                sanctioned=expression.get('sanctioned', False),
-                note=expression.get('note'),
-                source=expression.get('source'),
-                concept=c,
-                expression=l)
-            l.multilingualconcepts.add(m)
 
 def run():
     for m, concept in make_m():
