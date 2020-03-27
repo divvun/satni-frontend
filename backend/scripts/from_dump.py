4588f2a405377a8204184c1a3ca4d7e07b20ea5f
Børre Gaup <albbas@gmail.com>
2020-03-27 16:07:23 +0100

    Moved code into separate functions

--- backend/scripts/from_dump.py
+++ backend/scripts/from_dump.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-from termwikiimporter import bot
 from lemmas.models import Lemma
 from terms.models import Concept, MultiLingualConcept, Term
+from termwikiimporter import bot
 
 langs = {
     'en': 'eng',
@@ -22,17 +22,14 @@ langs = {
 def make_concept(lang, concept_infos, m):
     for concept_info in concept_infos:
         if lang == concept_info['language']:
-                return Concept.objects.create(
-                    language=langs[lang],
-                    definition=concept_info.get('definition'),
-                    explanation=concept_info.get('explanation'),
-                    multilingualconcepts=m
-                )
+            return Concept.objects.create(
+                language=langs[lang],
+                definition=concept_info.get('definition'),
+                explanation=concept_info.get('explanation'),
+                multilingualconcepts=m)
     else:
         return Concept.objects.create(
-            language=langs[lang],
-            multilingualconcepts=m
-        )
+            language=langs[lang], multilingualconcepts=m)
 
 
 def same_lang_expressions(lang, expressions):
@@ -41,35 +38,36 @@ def same_lang_expressions(lang, expressions):
         if expression['language'] == lang
     ]
 
-
-def run():
+def make_m():
     dumphandler = bot.DumpHandler()
-    titles = set()
 
-    for x, (title, concept) in dumphandler.concepts:
+
+    for x, (title, concept) in enumerate(dumphandler.concepts):
         print(f'Adding {x} {title}')
-        if x > 10000:
+        if x > 100:
             break
         x += 1
-        if title not in titles:
-            titles.add(title)
-            m, _ = MultiLingualConcept.objects.get_or_create(name=f'{title}')
+        m, _ = MultiLingualConcept.objects.get_or_create(name=f'{title}')
+        yield m, concept
 
-            for lang in concept.languages():
-                c = make_concept(lang, concept.data['concept_infos'], m)
-                c.save()
-                for expression in same_lang_expressions(lang, concept.related_expressions):
-                    l, _ = Lemma.objects.get_or_create(
-                        lemma=expression['expression'],
-                        pos=expression['pos'],
-                        language=langs[lang]
-                    )
-                    term = Term.objects.create(
-                        status=expression.get('status'),
-                        sanctioned=expression.get('sanctioned', False),
-                        note=expression.get('note'),
-                        source=expression.get('source'),
-                        concept=c,
-                        expression=l
-                    )
-                    l.multilingualconcepts.add(m)
+def make_c(m, concept):
+    for lang in concept.languages():
+        c = make_concept(lang, concept.data['concept_infos'], m)
+        for expression in same_lang_expressions(
+                lang, concept.related_expressions):
+            l, _ = Lemma.objects.get_or_create(
+                lemma=expression['expression'],
+                pos=expression['pos'],
+                language=langs[lang])
+            term = Term.objects.create(
+                status=expression.get('status'),
+                sanctioned=expression.get('sanctioned', False),
+                note=expression.get('note'),
+                source=expression.get('source'),
+                concept=c,
+                expression=l)
+            l.multilingualconcepts.add(m)
+
+def run():
+    for m, concept in make_m():
+        make_c(m, concept)
