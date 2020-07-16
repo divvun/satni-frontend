29ceeb5a4a5bbfda25f4d6b265fae488da2db5f4
Børre Gaup <albbas@gmail.com>
2020-07-16 22:15:46 +0200

    Make wantedLangs globally available
    Support all the languages available in TermWiki

--- frontend/src/containers/Root.js
+++ frontend/src/containers/Root.js
@@ -4,25 +4,39 @@ import { Provider } from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import AsyncApp from './AsyncApp';
 import ErrorBoundary from '../components/ErrorBoundary';
+import { resolvers, typeDefs } from '../resolvers';
 
+const cache = new InMemoryCache();
 const client = new ApolloClient({
-  uri: 'http://127.0.0.1:8000/graphql/'
+  cache,
+  uri: 'http://127.0.0.1:8000/graphql/',
+  resolvers,
+  typeDefs
+});
+cache.writeData({
+  data: {
+    wantedLangs: [
+      'sma', 'sme', 'smj', 'smn', 'sms',
+      'fin', 'nob', 'swe', 'nno',
+      'eng', 'lat'
+    ]
+  }
 });
 
 // Add this after Provider
 // <ErrorBoundary>
 // </ErrorBoundary>
 
-
 const Root = ({ store }) => (
   <Provider store={store}>
     <MuiThemeProvider>
-        <ApolloProvider client={client}>
-          <AsyncApp />
-        </ApolloProvider>
+      <ApolloProvider client={client}>
+        <AsyncApp />
+      </ApolloProvider>
     </MuiThemeProvider>
   </Provider>
     );
