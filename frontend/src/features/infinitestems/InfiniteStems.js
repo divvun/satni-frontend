08ed90d6f030ba9a815195436d5d5d95484f1a02
Børre Gaup <albbas@gmail.com>
2020-07-24 14:32:08 +0200

    Fetch items when reaching end of list

--- frontend/src/features/infinitestems/InfiniteStems.js
+++ frontend/src/features/infinitestems/InfiniteStems.js
@@ -4,49 +4,77 @@ import useStems from './InfiniteStems.hooks'
 import { useCookies } from 'react-cookie';
 import {gql} from 'apollo-boost';
 import { Trans } from '@lingui/macro';
-import Button from '@material-ui/core/Button';
+import AutoSizer from 'react-virtualized-auto-sizer';
+import InfiniteLoader from 'react-window-infinite-loader'
+import { FixedSizeList as List } from 'react-window'
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { makeStyles } from '@material-ui/core/styles';
+import { Redirect } from 'react-router-dom';
 
-const GET_LEMMAS = gql`
-  query AllLemmas($inputValue: String!, $wantedLangs: [String]!,
-                  $wantedDicts: [String]!, $after: String ) {
-      stemList(first:10,
-               search: $inputValue,
-               wanted: $wantedLangs,
-               wantedDicts: $wantedDicts
-               after: $after
-             ) {
-      edges {
-        node {
-          stem
-          srclangs
-          targetlangs
-        }
-      }
-      pageInfo {
-        endCursor
-        hasNextPage
-      }
-    }
+const useStyles = makeStyles(theme => ({
+  infiniteList: {
+    flexGrow: 1,
+    minHeight: 500,
+    maxWidth: 400
   }
-`;
+}))
+
+const redirect = (stem) => <Redirect to={`/article/${stem.stem}`} push />
 
 const InfiniteStems = () => {
-  const {data, loading, error, loadMore, hasNextPage} = useStems('s')
+  const {stems, loading, error, loadMore, hasNextPage} = useStems('gu')
+  const classes = useStyles();
 
-  if (loading) return <Trans><p>Loading...</p></Trans>;
-  if (error) return <Trans><p>Error {error.message}</p></Trans>;
-  if (!data) return <Trans><p>Not found</p></Trans>;
+  const stemsCount = hasNextPage ? stems.length + 1 : stems.length;
+  const loadMoreStems = loading ? () => {} : loadMore;
+  const isStemLoaded = index => !hasNextPage || index < stems.length;
+
+  if (loading && stems.length === 0) return <p>Loading ...</p>
+
   return (
-    <>
-      {data.stemList.edges.map(({node}, index) => <div key={index}>{node.stem}</div>)}
-      <Button
-        variant="contained"
-        onClick={() => loadMore()}
-      >
-        Default
-      </Button>
-    </>
-  )
+
+    <div className={classes.infiniteList}>
+      <AutoSizer>
+      {({height, width}) => (
+        <InfiniteLoader
+          isItemLoaded={isStemLoaded}
+          itemCount={stemsCount}
+          loadMoreItems={loadMoreStems}>
+          {({onItemsRendered, ref}) => (
+            <List
+              height={height}
+              itemCount={stemsCount}
+              itemSize={20}
+              onItemsRendered={onItemsRendered}
+              ref={ref}
+              width={width}>
+              {({index, style}) => {
+                let content;
+                if (!isStemLoaded(index)) {
+                  content = 'Loading...';
+                  return (
+                    <ListItem button key={index} style={style}>
+                      <ListItemText primary={content}/>
+                    </ListItem>
+                  )
+                } else {
+                  const {stem, srclangs} = stems[index]
+                  content = `${srclangs[0]} ${stem}`;
+                  return (
+                    <ListItem button key={index} style={style} component='a' href={`/article/${stem}`}>
+                      <ListItemText primary={content}/>
+                    </ListItem>
+                  )
+                }
+              }}
+            </List>
+          )}
+        </InfiniteLoader>
+      )}
+      </AutoSizer>
+    </div>
+  );
 }
 
 export default InfiniteStems
