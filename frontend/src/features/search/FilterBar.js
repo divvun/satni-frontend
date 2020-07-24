37599ea3073e0240717de4eb62a25ed9ecfbe5ec
Børre Gaup <albbas@gmail.com>
2020-07-25 01:06:54 +0200

    Increase the width of the search field

--- frontend/src/features/search/FilterBar.js
+++ frontend/src/features/search/FilterBar.js
@@ -5,6 +5,7 @@ import SearchIcon from '@material-ui/icons/Search';
 
 const useStyles = makeStyles((theme) => ({
   search: {
+    flex: 1,
     position: 'relative',
     borderRadius: theme.shape.borderRadius,
     backgroundColor: fade(theme.palette.common.white, 0.15),
