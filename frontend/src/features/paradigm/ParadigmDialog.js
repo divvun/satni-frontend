d1f07c456f7bd0a15d7d75031dc6f2aae372cf93
Børre Gaup <albbas@gmail.com>
2020-07-25 19:08:27 +0200

    Show paradigm as a dialog available from a button behind the lemma

---
+++ frontend/src/features/paradigm/ParadigmDialog.js
@@ -0,0 +1,55 @@
+import React, { useEffect } from 'react';
+import Typography from '@material-ui/core/Typography';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Dialog from '@material-ui/core/Dialog';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import { makeStyles } from '@material-ui/core/styles';
+
+import Paradigm from './Paradigm'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(2),
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+}));
+
+const ParadigmDialog = (props) => {
+  const classes = useStyles()
+  const {lemma, pos, language, open, onClose} = props
+
+  const handleClose = () => {
+      onClose();
+  };
+
+  return (
+    <Dialog onClose={handleClose} aria-labelledby="Paradigm" open={open}>
+      <DialogTitle id="simple-dialog-title">
+        <Typography variant="h6">
+          Paradigm for {lemma} {pos} {language}
+        </Typography>
+        {onClose ? (
+          <IconButton
+              aria-label="close"
+              onClick={onClose}
+              className={classes.closeButton}>
+            <CloseIcon />
+          </IconButton>
+        ) : null}
+      </DialogTitle>
+      <Paradigm
+        lemma={lemma}
+        language={language}
+        pos={pos}/>
+    </Dialog>
+  )
+}
+
+export default ParadigmDialog;
