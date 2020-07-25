6f1ebdc433d54589150821f2da21342f271deee9
Børre Gaup <albbas@gmail.com>
2020-07-25 12:59:06 +0200

    Setup linguijs support for storybook

--- frontend/.storybook/config.js
+++ frontend/.storybook/config.js
@@ -1,4 +1,24 @@
-import { configure } from '@storybook/react';
+import { addDecorator, configure } from '@storybook/react';
+import { setLinguiConfig, withLingui } from "storybook-addon-linguijs";
+import catalogSe from '../src/locales/se/messages.js';
+import catalogNb from '../src/locales/nb/messages.js';
+import catalogEn from '../src/locales/en/messages.js';
+
+// Set configuration
+const catalogs = {
+  nb: catalogNb,
+  se: catalogSe,
+  en: catalogEn
+};
+
+setLinguiConfig({
+  locales: ["en", "nb", "se"],
+  defaultLocale: "se",
+  catalogs
+});
+
+// Register decorator
+addDecorator(withLingui);
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../src', true, /\.stories\.js$/);
