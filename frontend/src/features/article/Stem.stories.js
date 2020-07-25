cfa372d8bf681b6e4ae92bd247e1ba504ab0a894
Børre Gaup <albbas@gmail.com>
2020-07-25 13:07:22 +0200

    Restriction is an object

--- frontend/src/features/article/Stem.stories.js
+++ frontend/src/features/article/Stem.stories.js
@@ -4,6 +4,9 @@ import { storiesOf } from '@storybook/react';
 import Stem from './Stem';
 
 const stem = { id: '68360', lemma: 'bárdni', language: 'sme', pos: 'N' };
-const restriction="Om julenissen"
+const restriction = {
+  restriction: 'om julenissen',
+  attributes: {}
+}
 storiesOf('Stem', module)
   .add('default', () => <Stem stem={stem} restriction={restriction}/>);
