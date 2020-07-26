bc84e4b512e9647b7611beecce82055ac9bd80f7
Børre Gaup <albbas@gmail.com>
2020-07-26 13:41:51 +0200

    Check if a stem exists before making a link out of it

--- frontend/src/features/article/Stem.js
+++ frontend/src/features/article/Stem.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import {gql} from 'apollo-boost';
+import {useQuery} from '@apollo/react-hooks';
 import { makeStyles } from '@material-ui/core/styles';
 import Link from '@material-ui/core/Link';
-import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import InfoOutlined from '@material-ui/icons/InfoOutlined'
 import ViewHeadlineOutlined from '@material-ui/icons/ViewHeadlineOutlined'
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 import ParadigmDialog from 'features/paradigm/ParadigmDialog'
+
+const HAS_STEM = gql`
+  query HasStem($stem: String!) {
+    hasStem(exact: $stem) {
+      stem
+    }
+  }
+`
 
 const useStyles = makeStyles({
   stemContainer: {
@@ -37,6 +49,13 @@ const Stem = ({ stem, restriction }) => {
   const {lemma, pos, language} = stem;
   const [openParadigm, setOpenParadigm] = React.useState(false);
   const classes = useStyles();
+  const {data, loading, error} = useQuery(
+    HAS_STEM, {
+      variables: {
+        stem: lemma
+      }
+    }
+  )
 
   const handleClickParadigmDialog = () => {
     setOpenParadigm(true)
@@ -46,9 +65,14 @@ const Stem = ({ stem, restriction }) => {
     setOpenParadigm(false)
   }
 
+  if (loading) return <CircularProgress className={classes.progress} size={20}/>
+
   return (
     <Typography className={classes.stemContainer}>
-      <Typography component='span' className={classes.lemma}>{lemma}</Typography>
+      {(!data.hasStem.length || error) ?
+        <Typography component='span' className={classes.lemma}>Not stem {lemma}</Typography> :
+        <Typography component='span' className={classes.lemma}><Link href="#">{lemma}</Link></Typography>
+      }
       {restriction && <Typography component='span'> ({restriction.restriction})</Typography>}
       <span className={classes.rightGroup}>
         {pos && <Typography component='span' color='textSecondary' className={classes.pos}>({pos})</Typography>}
