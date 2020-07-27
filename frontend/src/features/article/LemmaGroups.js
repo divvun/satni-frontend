a2c777be2c90362e5b0a343428218755c96b9cc8
Børre Gaup <albbas@gmail.com>
2020-07-27 14:28:21 +0200

    Always link target language entries of TermWiki

--- frontend/src/features/article/LemmaGroups.js
+++ frontend/src/features/article/LemmaGroups.js
@@ -5,10 +5,15 @@ import LemmaGroup from './LemmaGroup';
 const LemmaGroups = ({ fromterms, toterms }) => (
   <Grid container spacing={1}>
     <Grid item xs={6}>
-      {fromterms.map((fromterm, index) => <LemmaGroup key={index} term={fromterm} />)}
+      {fromterms.map((fromterm, index) => <LemmaGroup
+        key={index}
+        term={fromterm} />)}
     </Grid>
     <Grid item xs={6}>
-      {toterms.map((toterm, index) => <LemmaGroup key={index} term={toterm} />)}
+      {toterms.map((toterm, index) => <LemmaGroup
+        key={index}
+        term={toterm}
+        withLink />)}
     </Grid>
   </Grid>
 );
