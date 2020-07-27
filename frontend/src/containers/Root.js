8f6beda787e92f0ee29ed688bf6ddcf093aca793
Børre Gaup <albbas@gmail.com>
2020-07-27 16:01:48 +0200

    Update to apollo-client 3
    Means removing apollo-boost, @apollo/react-hooks and
    @apollo/react-components

--- frontend/src/containers/Root.js
+++ frontend/src/containers/Root.js
@@ -2,9 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from '@apollo/react-hooks';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { useCookies } from 'react-cookie';
 import { I18nProvider } from '@lingui/react';
 
