180bed44e5badac447502cba40de81d9dc00616a
Børre Gaup <albbas@gmail.com>
2020-07-27 13:18:21 +0200

    Move the query out of Stem

--- frontend/src/features/article/Stem.js
+++ frontend/src/features/article/Stem.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import {gql} from 'apollo-boost';
-import {useQuery} from '@apollo/react-hooks';
 import { makeStyles } from '@material-ui/core/styles';
 import Link from '@material-ui/core/Link';
 import IconButton from '@material-ui/core/IconButton';
@@ -11,14 +9,6 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 import ParadigmDialog from 'features/paradigm/ParadigmDialog';
-
-const HAS_STEM = gql`
-  query HasStem($stem: String!) {
-    hasStem(exact: $stem) {
-      stem
-    }
-  }
-`;
 
 const useStyles = makeStyles({
   stemContainer: {
@@ -45,17 +35,10 @@ const useStyles = makeStyles({
   }
 });
 
-const Stem = ({ stem, restriction }) => {
+const Stem = ({ stem, restriction, withLink }) => {
+  const classes = useStyles();
   const {lemma, pos, language} = stem;
   const [openParadigm, setOpenParadigm] = React.useState(false);
-  const classes = useStyles();
-  const {data, loading, error} = useQuery(
-    HAS_STEM, {
-      variables: {
-        stem: lemma
-      }
-    }
-  );
 
   const handleClickParadigmDialog = () => {
     setOpenParadigm(true);
@@ -65,22 +48,24 @@ const Stem = ({ stem, restriction }) => {
     setOpenParadigm(false);
   };
 
-  if (loading) return <CircularProgress className={classes.progress} size={20}/>;
-
   return (
     <Typography className={classes.stemContainer}>
-      {(!data.hasStem.length || error) ?
-        <Typography component='span' className={classes.lemma}>Not stem {lemma}</Typography> :
-        <Typography component='span' className={classes.lemma}><Link href="#">{lemma}</Link></Typography>
-      }
+      <Typography
+        component='span'
+        className={classes.lemma}>
+        {(withLink) ?
+          <Link href='#'>{lemma}</Link> :
+          <>{lemma}</>
+        }
+      </Typography>
       {restriction && <Typography component='span'> ({restriction.restriction})</Typography>}
       <span className={classes.rightGroup}>
         {pos && <Typography component='span' color='textSecondary' className={classes.pos}>({pos})</Typography>}
-        <IconButton className={classes.icons} component='span' aria-label="Paradigm" onClick={handleClickParadigmDialog}>
-          <InfoOutlined/>
+        <IconButton className={classes.icons} component='span' aria-label='Paradigm' onClick={handleClickParadigmDialog}>
+          <InfoOutlined />
         </IconButton>
-        <IconButton className={classes.icons} component='span' aria-label="Corpus">
-          <ViewHeadlineOutlined/>
+        <IconButton className={classes.icons} component='span' aria-label='Corpus'>
+          <ViewHeadlineOutlined />
         </IconButton>
       </span>
       <ParadigmDialog
