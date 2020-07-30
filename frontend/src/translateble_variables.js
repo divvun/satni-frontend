6f6afe89f429ed03f6946a0c41b27e49acc509a8
Børre Gaup <albbas@gmail.com>
2020-07-30 14:51:45 +0200

    Translatable variables and some translations

---
+++ frontend/src/translateble_variables.js
@@ -0,0 +1,13 @@
+import { t } from '@lingui/macro';
+
+const dicts = [
+  t`termwiki`, t`gtsmenob`, t`gtnobsme`, t`gtnobsma`, t`gtsmanob`, t`gtsmefin`,
+  t`gtfinsme`, t`gtsmesmn`, t`gtsmnsme`, t`pksmefin`
+];
+
+const partOfSpeech = [ t`N`, t`V`, t`A`, t`Adv`, t`Pron`];
+
+const languageCodes = [
+  t`en`, t`eng`, t`fi`, t`fin`, t`nb`, t`nn`, t`nno`, t`nob`, t`se`, t`sv`,
+  t`sma`, t`sme`, t`smj`, t`smn`, t`sms`, t`swe`, t`lat`
+];
