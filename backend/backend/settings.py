a6867305cca3753db03d4ef7451161442cccd758
Børre Gaup <albbas@gmail.com>
2020-08-01 23:33:41 +0200

    Set proxy to and uri to query the backend directly

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -139,9 +139,6 @@ GRAPHENE = {
     'SCHEMA': 'backend.schema.schema',
 }
 
-CORS_ORIGIN_ALLOW_ALL = True
-
-#CORS_ORIGIN_WHITELIST = (
-#'http://localhost:8000',
-#'http://127.0.0.1:8000',
-#)
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:8000'
+]
