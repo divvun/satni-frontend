636eab3d0680e1ca1c14bf9621353c5d3505ccac
Børre Gaup <albbas@gmail.com>
2020-08-18 14:38:14 +0200

    New translatable text

--- frontend/src/translateble_variables.js
+++ frontend/src/translateble_variables.js
@@ -11,3 +11,11 @@ const languageCodes = [
   t`en`, t`eng`, t`fi`, t`fin`, t`nb`, t`nn`, t`nno`, t`nob`, t`se`, t`sv`,
   t`sma`, t`sme`, t`smj`, t`smn`, t`sms`, t`swe`, t`lat`
 ];
+
+const languageCodesGenitive = [
+  t`en_genitive`, t`eng_genitive`, t`fi_genitive`, t`fin_genitive`,
+  t`nb_genitive`, t`nn_genitive`, t`nno_genitive`, t`nob_genitive`,
+  t`se_genitive`, t`sv_genitive`, t`sma_genitive`, t`sme_genitive`,
+  t`smj_genitive`, t`smn_genitive`, t`sms_genitive`, t`swe_genitive`,
+  t`lat_genitive`
+];
