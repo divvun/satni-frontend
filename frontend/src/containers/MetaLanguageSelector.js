8eea651a19cdfcd0e8ad7108865a8d8955afa983
Børre Gaup <albbas@gmail.com>
2020-08-19 12:27:55 +0200

    Move the page language selector to a separate file

---
+++ frontend/src/containers/MetaLanguageSelector.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+
+const MetaLanguageSelector = ({ language, onChangeLangage }) => {
+  const handleChange = (event) => {
+    event.preventDefault();
+    onChangeLangage(event.target.value);
+  };
+
+  return (
+    <FormControl variant='filled'>
+      <InputLabel id='page-language-label'>Page language</InputLabel>
+      <Select
+        labelId='page-language-label'
+        id='page-language'
+        value={language}
+        onChange={handleChange}
+        label='Page language'
+      >
+        <MenuItem value='en'>English</MenuItem>
+        <MenuItem value='se'>Davvisámegiella</MenuItem>
+        <MenuItem value='smj'>Julevsámegiella</MenuItem>
+        <MenuItem value='nb'>Bokmål</MenuItem>
+      </Select>
+    </FormControl>
+  );
+};
+
+export default MetaLanguageSelector;
