6b6144f904fa64c6a79d73e462a29ad00855fc2d
Børre Gaup <albbas@gmail.com>
2020-08-19 13:16:44 +0200

    Move the appbar to a separate file

---
+++ frontend/src/containers/SatniAppBar.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+import FilterBar from 'features/search/FilterBar';
+
+const SatniAppBar = (props) => {
+  const { handleSearch, handleDrawerToggle, drawerWidth } = props;
+  const useStyles = makeStyles(theme => ({
+    appBar: {
+      position: 'relative',
+      flexDirection: 'column',
+      [theme.breakpoints.up('sm')]: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth
+      }
+    }
+  }));
+  const classes = useStyles();
+
+  return (
+    <AppBar position='static' className={classes.appBar}>
+      <Toolbar>
+        <IconButton
+          color='inherit'
+          aria-label='open drawer'
+          edge='start'
+          onClick={handleDrawerToggle}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant='h6' noWrap>
+          sátni.org
+        </Typography>
+        <FilterBar searchHandler={handleSearch} />
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default SatniAppBar;
