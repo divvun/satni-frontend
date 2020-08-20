a232f38f47c4beae69e05d21497531a020ea494c
Børre Gaup <albbas@gmail.com>
2020-08-21 01:03:01 +0200

    More intro text and info on the dictionaries

--- frontend/src/containers/Welcome.js
+++ frontend/src/containers/Welcome.js
@@ -3,14 +3,16 @@ import { makeStyles } from '@material-ui/core/styles';
 
 import { Trans } from '@lingui/macro';
 import InfoOutlined from '@material-ui/icons/InfoOutlined';
+import KeyboardIcon from '@material-ui/icons/Keyboard';
 import LanguageIcon from '@material-ui/icons/Language';
 import MenuBookIcon from '@material-ui/icons/MenuBook';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import ViewHeadlineOutlined from '@material-ui/icons/ViewHeadlineOutlined';
 
-import LangChooser from 'features/wantedlangs/LangChooser';
 import DictChooser from 'features/wantedlangs/DictChooser';
+import DictionaryInfo from './DictionaryInfo';
+import LangChooser from 'features/wantedlangs/LangChooser';
 
 const useStyles = makeStyles(theme => ({
   welcome: {
@@ -38,7 +40,7 @@ export const DictWelcome = () => {
         className={classes.heading}
       >
         <Trans>
-        Welcome!
+          Welcome!
         </Trans>
       </Typography>
       <Typography
@@ -46,7 +48,7 @@ export const DictWelcome = () => {
         className={classes.list}
       >
         <Trans>
-        Dictionary articles will appear in this pane when you press a word in the search results to the left.
+          Here you will find <a href='https://giella.org'>Giellagáldu's</a> term collection, Pekka Sammallahti's North Sami – Finnish dictionary and <a href='http://giellatekno.uit.no'>Giellatekno's</a> sami dictionaries, all set up by <a href='http://divvun.no'>Divvun</a>.
         </Trans>
       </Typography>
       <Typography
@@ -54,7 +56,7 @@ export const DictWelcome = () => {
         className={classes.list}
       >
         <Trans>
-        Click <InfoOutlined className={classes.icons} /> to see the inflection paradigm of words. Click <ViewHeadlineOutlined className={classes.icons} /> to see examples of how the word is used.
+          Dictionary articles will appear in this pane when you press a word in the search results to the left.
         </Trans>
       </Typography>
       <Typography
@@ -62,9 +64,18 @@ export const DictWelcome = () => {
         className={classes.list}
       >
         <Trans>
-        Inside the articles you can hide unwanted languages and dictionaries. To reactivate them, press <LanguageIcon className={classes.icons} /> or <MenuBookIcon className={classes.icons} /> .
+          Click <InfoOutlined className={classes.icons} /> to see the inflection paradigm of words. Click <ViewHeadlineOutlined className={classes.icons} /> to see examples of how the word is used.
         </Trans>
       </Typography>
+      <Typography
+        component='p'
+        className={classes.list}
+      >
+        <Trans>
+          Inside the articles you can hide unwanted languages and dictionaries. To reactivate them, press <LanguageIcon className={classes.icons} /> or <MenuBookIcon className={classes.icons} /> .
+        </Trans>
+      </Typography>
+      <DictionaryInfo />
     </div>
   );
 };
@@ -79,7 +90,7 @@ export const SearchWelcome = () => {
         className={classes.list}
       >
         <Trans>
-        Search results will appear here.
+          Search results will appear here.
         </Trans>
       </Typography>
       <Typography
@@ -87,7 +98,8 @@ export const SearchWelcome = () => {
         className={classes.list}
       >
         <Trans>
-        The input field accepts sami, nordic, english and latin search words.
+          The input field accepts sami, nordic, english and latin search words.
+          If you do not have Sami letters on your device, press <KeyboardIcon className={classes.icons} /> to write them in the input field.
         </Trans>
       </Typography>
       <Typography
@@ -95,7 +107,7 @@ export const SearchWelcome = () => {
         className={classes.list}
       >
         <Trans>
-        You can browse the search words here, new ones will be fetched until all matches have been found.
+          You can browse the search words here, new ones will be fetched until all matches have been found.
         </Trans>
       </Typography>
       <Typography
@@ -103,29 +115,9 @@ export const SearchWelcome = () => {
         className={classes.list}
       >
         <Trans>
-        When you click a word here, dictionary articles appear to the right.
+          When you click a word here, dictionary articles appear to the right.
         </Trans>
       </Typography>
     </div>
   );
 };
-// <div className={classes.welcome}>
-//   <Typography
-//     variant='h4'
-//     className={classes.heading}
-//   >
-//     Velkommen!
-//   </Typography>
-//   <Typography
-//     component='p'
-//     className={classes.list}
-//   >
-//     Her vises ordboksartikler. Du kan velge bort språk og ordbøker du ikke vil se i artiklene, eller ved å trykke <LanguageIcon /> og <MenuBookIcon />.
-//   </Typography>
-//   <Typography
-//     component='p'
-//     className={classes.list}
-//   >
-//     Trykk på <InfoOutlined /> får å se bøyingsmønsteret. Trykk på <ViewHeadlineOutlined /> for å se eksempler på hvordan ordet er brukt.
-//   </Typography>
-// </div>
