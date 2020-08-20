603329abedb3c9a1fad3e7c1ebdf45a00167452d
Børre Gaup <albbas@gmail.com>
2020-08-20 12:40:10 +0200

    Move availableDicts to utils

--- frontend/src/features/wantedlangs/DictChooser.js
+++ frontend/src/features/wantedlangs/DictChooser.js
@@ -5,11 +5,10 @@ import Checkbox from '@material-ui/core/Checkbox';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 
+import { availableDicts } from 'utils';
+
 const DictChooser = () => {
   const [cookies, setCookie] = useCookies(['wantedDicts']);
-  const availableDicts = [
-    'termwiki', 'gtsmenob', 'gtnobsme', 'gtnobsma', 'gtsmanob', 'gtsmefin', 'gtfinsme', 'gtsmesmn', 'gtsmnsme', 'sammallahtismefin'
-  ];
 
   if (cookies.wantedDicts === undefined) {
     setCookie('wantedDicts', availableDicts);
