5325f8aa61e765f7ca0efd1fe226a284010de07b
Børre Gaup <albbas@gmail.com>
2020-08-22 10:22:49 +0200

    Fullscreen Dict dialog

--- frontend/src/features/wantedlangs/DictChooserDialog.js
+++ frontend/src/features/wantedlangs/DictChooserDialog.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Trans } from '@lingui/macro';
+import AppBar from '@material-ui/core/AppBar';
 import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import CloseIcon from '@material-ui/icons/Close';
 import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import IconButton from '@material-ui/core/IconButton';
+import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
 import DictChooser from './DictChooser';
@@ -14,6 +21,13 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: theme.spacing(3),
     marginRight: theme.spacing(8),
     marginLeft: theme.spacing(8)
+  },
+  appBar: {
+    position: 'relative'
+  },
+  title: {
+    marginLeft: theme.spacing(2),
+    flex: 1
   }
 }));
 
@@ -26,12 +40,24 @@ const DictChooserDialog = (props) => {
   };
 
   return (
-    <Dialog onClose={handleClose} aria-labelledby='DictChooser' open={open}>
+    <Dialog fullScreen onClose={handleClose} aria-labelledby='DictChooser' open={open}>
+      <AppBar className={classes.appBar}>
+        <Toolbar>
+          <IconButton edge='start' color='inherit' onClick={handleClose} aria-label='close'>
+            <CloseIcon />
+          </IconButton>
+          <Typography variant='h6' className={classes.title}>
+            <Trans>Choose dictionaries</Trans>
+          </Typography>
+          <Button autoFocus color='inherit' onClick={handleClose}>
+          save
+          </Button>
+        </Toolbar>
+      </AppBar>
       <Box className={classes.root}>
-        <DialogTitle id='simple-dialog-title'>
-          <Trans>Choose dictionaries</Trans>
-        </DialogTitle>
-        <DictChooser />
+        <DialogContent>
+          <DictChooser />
+        </DialogContent>
       </Box>
     </Dialog>
   );
