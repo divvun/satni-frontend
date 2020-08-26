713d619d4b72b3a9b4b12f04e296e395318ff5ef
Børre Gaup <albbas@gmail.com>
2020-08-26 16:34:08 +0200

    Make it possible to disable a source in the article view

--- src/features/article/Source.js
+++ src/features/article/Source.js
@@ -2,29 +2,50 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 import { Trans } from '@lingui/macro';
+import { useCookies } from 'react-cookie';
+
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1
-  },
-  paper: {
-    padding: theme.spacing(1)
-  },
-  title: {
-    fontSize: 14
+  check: {
+    margin: 0,
+    paddingRight: 0
   }
 }));
 
 const Source = ({source, lemma}) => {
   const classes = useStyles();
+  const [cookies, setCookie] = useCookies(['wantedDicts']);
+
+  const handleChange = (event) => {
+    event.stopPropagation();
+    const oldDicts = cookies.wantedDicts;
+    const newDicts = oldDicts.includes(event.target.name) ?
+      oldDicts.filter(value => value !== event.target.name) :
+      [...oldDicts, event.target.name];
+    setCookie('wantedDicts', newDicts);
+  };
 
   return (
     <Grid item xs={12} className={classes.paper}>
-      <Typography>
-        <Trans>Source:</Trans> <Link to={`${source}/${lemma}`}><Trans id={source} /></Link>
-      </Typography>
+      <FormControlLabel
+        label={<Typography variant='body2'
+        >
+          <Trans>Source:</Trans> <Link to={`${source}/${lemma}`}><Trans id={source} /></Link>
+        </Typography>}
+        control={
+          <Checkbox
+            checked
+            className={classes.check}
+            color='default'
+            name={source}
+            onChange={handleChange}
+          />
+        }
+      />
     </Grid>
   );
 };
