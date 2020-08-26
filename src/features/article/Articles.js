cb83bbe0dd1930ebccdb58c84935fa0b091a004f
Børre Gaup <albbas@gmail.com>
2020-08-26 17:20:54 +0200

    Suggest to check settings when there are no results for a lemma lookup

--- src/features/article/Articles.js
+++ src/features/article/Articles.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import { Trans } from '@lingui/macro';
+import { useQuery } from '@apollo/client';
+
 import gql from 'graphql-tag';
+import LanguageIcon from '@material-ui/icons/Language';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
+import Typography from '@material-ui/core/Typography';
+
 import { elemmas2ConceptPairs, dictBackend2Frontend } from 'utils';
 import PresentArticles from './PresentArticles';
 import FetchArticlesError from './FetchArticlesError';
-import { useQuery } from '@apollo/client';
 
 const query2articlelist = (lemma, data) => {
   const termList = elemmas2ConceptPairs(lemma, data.conceptList);
@@ -12,8 +18,16 @@ const query2articlelist = (lemma, data) => {
   return dictList.concat(termList);
 };
 
+const useStyles = makeStyles(theme => ({
+  icons: {
+    fontSize: 'inherit'
+  }
+}));
+
 const Articles = (props) => {
   const {lemma, wantedDicts, wantedLangs} = props;
+  const classes = useStyles();
+
   const GET_ARTICLES = wantedDicts.includes('termwiki') ?
     gql`
       query AllArticles($lemma: String!, $wantedLangs: [String]!, $wantedDicts: [String]!) {
@@ -118,6 +132,17 @@ const Articles = (props) => {
   if (loading) return <Trans><p>Loading...</p></Trans>;
   if (error) return <Trans><p>Error {error.message}</p></Trans>;
   if (!data) return <Trans><p>Not found</p></Trans>;
+
+  if (!data.dictEntryList.length && !data.conceptList.length) {
+    return <Typography
+      component='p'
+      className={classes.list}
+    >
+      <Trans>
+      No results for <b>{lemma}</b>. Check <LanguageIcon className={classes.icons} /> or <MenuBookIcon className={classes.icons} /> in the left menu to see if the wanted languages or dictionaries are selected.
+      </Trans>
+    </Typography>;
+  }
 
   if (wantedDicts.includes('termwiki')) {
     return <PresentArticles
