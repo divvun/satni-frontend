4c11065c6ea477a9021b725957ebf3c3733e012a
Børre Gaup <albbas@gmail.com>
2020-08-27 02:01:05 +0200

    Use Route and Switch to catch old paths

--- src/containers/AsyncApp.js
+++ src/containers/AsyncApp.js
@@ -1,15 +1,21 @@
 import React, { useState } from 'react';
 import { useCookies } from 'react-cookie';
-import { useLocation } from 'react-router-dom';
+import {
+  Redirect,
+  Route,
+  Switch,
+  useLocation
+} from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import PropTypes from 'prop-types';
+import qs from 'qs';
 
 import { locationParser } from 'utils';
-import { DictWelcome, SearchWelcome, WelcomeHeader } from 'containers/Welcome';
 import Articles from 'features/article/Articles';
+import { DictWelcome, SearchWelcome, WelcomeHeader } from 'containers/Welcome';
 import InfiniteStems from 'features/infinitestems/InfiniteStems';
 import SatniAppBar from './SatniAppBar';
 import SatniDrawer from './SatniDrawer';
@@ -41,9 +47,11 @@ const styles = theme => ({
   }
 });
 
-const AsyncApp = ({classes, match, language, onLanguageChange}) => {
+const AsyncApp = ({classes, language, onLanguageChange}) => {
   const [searchExpression, setSearchExpression] = useState('');
-  const {currentLemma, currentDict} = locationParser(useLocation().pathname);
+  const location = useLocation();
+  const {currentLemma, currentDict} = locationParser(location.pathname);
+  const location_dict = qs.parse(location.search.slice(1));
 
   const handleSearch = value => setSearchExpression(value);
 
@@ -77,41 +85,51 @@ const AsyncApp = ({classes, match, language, onLanguageChange}) => {
         handleSearch={handleSearch}
       />
       <main className={classes.main}>
-        <Grid container>
-          <Grid
-            item
-            xs={12}
-          >
-            {(currentLemma || searchExpression) ?
-              <StatusBar
-                wantedDicts={wantedDicts}
-                wantedLangs={cookies.wantedLangs}
-                currentLemma={currentLemma} /> :
-              <WelcomeHeader />
-            }
-          </Grid>
-          <Grid item xs={4}>
-            {searchExpression ?
-              <InfiniteStems
-                searchExpression={searchExpression}
-                wantedDicts={wantedDicts}
-                wantedLangs={cookies.wantedLangs}
-                currentDict={currentDict}
-              /> :
-              <SearchWelcome />
-            }
-          </Grid>
-          <Grid item xs={8}>
-            {currentLemma ?
-              <Articles
-                lemma={currentLemma}
-                wantedDicts={wantedDicts}
-                wantedLangs={cookies.wantedLangs}
-              /> :
-              <DictWelcome />
-            }
-          </Grid>
-        </Grid>
+        <Switch>
+          <Redirect
+            from={`/article/${currentLemma}`}
+            to={`/${currentLemma}`} />
+          <Redirect
+            from={'/details'}
+            to={`/${location_dict.lemma}`} />
+          <Route path='/'>
+            <Grid container>
+              <Grid
+                item
+                xs={12}
+              >
+                {(currentLemma || searchExpression) ?
+                  <StatusBar
+                    wantedDicts={wantedDicts}
+                    wantedLangs={cookies.wantedLangs}
+                    currentLemma={currentLemma} /> :
+                  <WelcomeHeader />
+                }
+              </Grid>
+              <Grid item xs={4}>
+                {searchExpression ?
+                  <InfiniteStems
+                    searchExpression={searchExpression}
+                    wantedDicts={wantedDicts}
+                    wantedLangs={cookies.wantedLangs}
+                    currentDict={currentDict}
+                  /> :
+                  <SearchWelcome />
+                }
+              </Grid>
+              <Grid item xs={8}>
+                {currentLemma ?
+                  <Articles
+                    lemma={currentLemma}
+                    wantedDicts={wantedDicts}
+                    wantedLangs={cookies.wantedLangs}
+                  /> :
+                  <DictWelcome />
+                }
+              </Grid>
+            </Grid>
+          </Route>
+        </Switch>
       </main>
     </div>
   );
