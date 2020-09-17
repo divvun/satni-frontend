1ea50ef77098e09b4226d0107d88f4b595392615
Børre Gaup <albbas@gmail.com>
2020-09-17 17:15:42 +0200

    If dialect or country is available, show them

--- src/features/article/Stem.js
+++ src/features/article/Stem.js
@@ -36,7 +36,7 @@ const useStyles = makeStyles({
 
 const Stem = ({ stem, restriction, withLink, lemmaHandler }) => {
   const classes = useStyles();
-  const {lemma, pos, language} = stem;
+  const {lemma, pos, language, dialect, country} = stem;
   const [openParadigm, setOpenParadigm] = useState(false);
 
   const handleClickParadigmDialog = () => {
@@ -48,47 +48,52 @@ const Stem = ({ stem, restriction, withLink, lemmaHandler }) => {
   };
 
   return (
-    <Typography className={classes.stemContainer}>
-      <Typography
-        component='span'
-        className={classes.lemma}>
-        {(withLink) ?
-          <Link to={`/${lemma}`}>
-            {lemma}
-          </Link> :
-          <>{lemma}</>
-        }
-      </Typography>
-      {restriction && <Typography component='span'> ({restriction.restriction})</Typography>}
-      {!lemma.includes(' ') && <>
-        <span className={classes.rightGroup}>
-          {pos && <Typography
-            component='span'
-            color='textSecondary'
-            className={classes.pos}>(<Trans id={pos}/>)</Typography>}
-          <ParadigmButton
+    <>
+      <Typography className={classes.stemContainer}>
+        <Typography
+          component='span'
+          className={classes.lemma}>
+          {(withLink) ?
+            <Link to={`/${lemma}`}>
+              {lemma}
+            </Link> :
+            <>{lemma}</>
+          }
+        </Typography>
+        {restriction && <Typography component='span'> ({restriction.restriction})</Typography>}
+        {!lemma.includes(' ') && <>
+          <span className={classes.rightGroup}>
+            {pos && <Typography
+              component='span'
+              color='textSecondary'
+              className={classes.pos}>(<Trans id={pos}/>)</Typography>}
+            <ParadigmButton
+              lemma={lemma}
+              pos={pos}
+              language={language}
+              onClick={handleClickParadigmDialog}
+              classes={classes}
+            />
+            <KorpButton
+              lemma={lemma}
+              language={language}
+              classes={classes}
+            />
+          </span>
+          <ParadigmDialog
             lemma={lemma}
             pos={pos}
             language={language}
-            onClick={handleClickParadigmDialog}
-            classes={classes}
-          />
-          <KorpButton
-            lemma={lemma}
-            language={language}
-            classes={classes}
-          />
-        </span>
-        <ParadigmDialog
-          lemma={lemma}
-          pos={pos}
-          language={language}
-          open={openParadigm}
-          onClose={handleCloseParadigmDialog} />
-      </>
-      }
-
-    </Typography>
+            open={openParadigm}
+            onClose={handleCloseParadigmDialog} />
+        </>
+        }
+      </Typography>
+      {(dialect || country) && <Typography>
+        {dialect && <Typography component='span'>Dialect: {dialect} </Typography>}
+        {country && <Typography component='span'>Country: {country}</Typography>}
+      </Typography>}
+    </>
   );
 };
 
