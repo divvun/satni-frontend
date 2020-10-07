c0d2bdf5155da67f3b80fc833057ccbd4cce0ad1
Børre Gaup <albbas@gmail.com>
2020-10-07 16:38:28 +0200

    Do not wrap TermGroups in li elements

--- src/features/article/LemmaGroup.js
+++ src/features/article/LemmaGroup.js
@@ -5,14 +5,14 @@ import Typography from '@material-ui/core/Typography';
 import Stem from './Stem';
 
 const LemmaGroup = ({lemma, term}) => (
-  <li>
+  <>
     <Stem
       stem={term.expression}
       withLink={(term.expression.lemma !== lemma)} />
     {term.note && <Typography><Trans>Note:</Trans> {term.note}</Typography>}
     {term.status && <Typography><Trans>Status:</Trans> {term.status}</Typography>}
     {term.source && <Typography><Trans>Source:</Trans> {term.source}</Typography>}
-  </li>
+  </>
 );
 
 export default LemmaGroup;
