decd7cfd364914fac999f499960d2da8fb7b6db3
Børre Gaup <albbas@gmail.com>
2020-10-08 11:33:20 +0200

    Fix the layout of terms

--- src/features/article/LemmaGroup.js
+++ src/features/article/LemmaGroup.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Trans } from '@lingui/macro';
+import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 
 import Stem from './Stem';
 
-const LemmaGroup = ({lemma, term}) => (
-  <>
-    <Stem
-      stem={term.expression}
-      withLink={(term.expression.lemma !== lemma)} />
-    {term.note && <Typography><Trans>Note:</Trans> {term.note}</Typography>}
-    {term.status && <Typography><Trans>Status:</Trans> {term.status}</Typography>}
-    {term.source && <Typography><Trans>Source:</Trans> {term.source}</Typography>}
-  </>
-);
+const LemmaGroup = ({lemma, term}) => {
+  return (
+    <Box>
+      <Stem
+        stem={term.expression}
+        withLink={(term.expression.lemma !== lemma)} />
+      {term.note && <Typography><Trans>Note:</Trans> {term.note}</Typography>}
+      {term.status && <Typography><Trans>Status:</Trans> {term.status}</Typography>}
+      {term.source && <Typography><Trans>Source:</Trans> {term.source}</Typography>}
+    </Box>
+  );
+};
 
 export default LemmaGroup;
