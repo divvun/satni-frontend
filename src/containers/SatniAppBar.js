0a5a6ac0be5f8741c45a6243f17d3c4fac77ff62
Børre Gaup <albbas@gmail.com>
2020-10-20 10:09:22 +0200

    Make help page
    Closes #30

--- src/containers/SatniAppBar.js
+++ src/containers/SatniAppBar.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { Trans } from '@lingui/macro';
 import AppBar from '@material-ui/core/AppBar';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import PropTypes from 'prop-types';
 import Toolbar from '@material-ui/core/Toolbar';
+import Tooltip from '@material-ui/core/Tooltip';
 import Typography from '@material-ui/core/Typography';
 
 import FilterBar from 'features/search/FilterBar';
+import HelpDialog from 'containers/HelpDialog';
 
 const SatniAppBar = ({ handleSearch, handleDrawerToggle, drawerWidth }) => {
   const useStyles = makeStyles(theme => ({
@@ -19,6 +23,9 @@ const SatniAppBar = ({ handleSearch, handleDrawerToggle, drawerWidth }) => {
         marginLeft: drawerWidth
       }
     },
+    helpButton: {
+      paddingLeft: theme.spacing(4)
+    },
     menuButton: {
       marginRight: theme.spacing(2),
       [theme.breakpoints.up('md')]: {
@@ -28,23 +35,53 @@ const SatniAppBar = ({ handleSearch, handleDrawerToggle, drawerWidth }) => {
   }));
   const classes = useStyles();
 
+  const [openHelpDialog, setOpenHelpDialog] = useState(false);
+  const handleClickHelpDialog = () => {
+    setOpenHelpDialog(true);
+  };
+  const handleCloseHelpDialog = () => {
+    setOpenHelpDialog(false);
+  };
+
   return (
     <AppBar position='static' className={classes.appBar}>
       <Toolbar>
-        <IconButton
-          color='inherit'
-          aria-label='open drawer'
-          edge='start'
-          onClick={handleDrawerToggle}
-          className={classes.menuButton}
+        <Tooltip
+          title={<Trans>Settings</Trans>}
+          aria-label={<Trans>Settings</Trans>}
         >
-          <MenuIcon />
-        </IconButton>
+          <IconButton
+            color='inherit'
+            aria-label='Settings'
+            edge='start'
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Tooltip>
         <Typography variant='h6' noWrap>
           sátni.org
         </Typography>
         <FilterBar searchHandler={handleSearch} />
+        <Tooltip
+          title={<Trans>Help</Trans>}
+          aria-label={<Trans>Help</Trans>}
+        >
+          <IconButton
+            color='inherit'
+            aria-label='Settings'
+            edge='start'
+            onClick={handleClickHelpDialog}
+            className={classes.helpButton}
+          >
+            <HelpOutlineIcon />
+          </IconButton>
+        </Tooltip>
       </Toolbar>
+      <HelpDialog
+        open={openHelpDialog}
+        onClose={handleCloseHelpDialog} />
     </AppBar>
   );
 };
