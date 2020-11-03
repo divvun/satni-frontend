da277484442de4c822b6c4b3b32e70726f09dc15
Børre Gaup <albbas@gmail.com>
2020-11-03 17:17:20 +0100

    Fix the story to use real data

--- src/features/article/TermWikiArticle.stories.js
+++ src/features/article/TermWikiArticle.stories.js
@@ -1,158 +1,11 @@
 import React from 'react';
 
 import TermWikiArticle from './TermWikiArticle';
+import { multilingualconceptListsByNames, orderedMultilingualConcept } from 'utils';
+import { conceptList } from 'utils.data';
 
-const concepts = [
-  {
-    collections: ['Collection:SD-terms', 'Collection:Dáhtábágo javllamáno 2013'],
-    definition: null,
-    explanation: null,
-    terms: [
-      {
-        expression: {
-          language: 'smj',
-          lemma: 'fijlla',
-          pos: 'N',
-        },
-        note: 'jll jl',
-        source: null,
-        status: null,
-      },
-    ],
-  },
-  {
-    collections: ['Collection:SD-terms', 'Collection:Dáhtábágo javllamáno 2013'],
-    definition: null,
-    explanation: null,
-    terms: [
-      {
-        expression: {
-          language: 'fin',
-          lemma: 'viila',
-          pos: 'N',
-        },
-        note: null,
-        source: null,
-        status: null,
-      },
-      {
-        expression: {
-          language: 'fin',
-          lemma: 'kaista',
-          pos: 'N',
-        },
-        note: null,
-        source: null,
-        status: null,
-      },
-      {
-        expression: {
-          language: 'fin',
-          lemma: 'tiedosto',
-          pos: 'N',
-        },
-        note: null,
-        source: null,
-        status: null,
-      },
-    ],
-  },
-  {
-    collections: ['Collection:SD-terms', 'Collection:Dáhtábágo javllamáno 2013'],
-    definition: null,
-    explanation: null,
-    terms: [
-      {
-        expression: {
-          language: 'eng',
-          lemma: 'file',
-          pos: 'N',
-        },
-        note: null,
-        source: null,
-        status: null,
-      },
-    ],
-  },
-  {
-    collections: ['Collection:SD-terms', 'Collection:Dáhtábágo javllamáno 2013'],
-    definition: 'fil, i edb: en ordnet mengde av sammenhørende data',
-    explanation: null,
-    terms: [
-      {
-        expression: {
-          language: 'nob',
-          lemma: 'fil',
-          pos: 'N',
-        },
-        note: null,
-        source: null,
-        status: null,
-      },
-    ],
-  },
-  {
-    collections: ['Collection:SD-terms', 'Collection:Dáhtábágo javllamáno 2013'],
-    definition: null,
-    explanation: null,
-    terms: [
-      {
-        expression: {
-          language: 'swe',
-          lemma: 'fil',
-          pos: 'N',
-        },
-        note: null,
-        source: null,
-        status: null,
-      },
-    ],
-  },
-  {
-    collections: ['Collection:SD-terms', 'Collection:Dáhtábágo javllamáno 2013'],
-    definition: null,
-    explanation: null,
-    terms: [
-      {
-        expression: {
-          language: 'sme',
-          lemma: 'fiila',
-          pos: 'N',
-        },
-        note: null,
-        source: null,
-        status: null,
-      },
-      {
-        expression: {
-          language: 'sme',
-          lemma: 'vuorká',
-          pos: 'N',
-        },
-        note: null,
-        source: null,
-        status: null,
-      },
-    ],
-  },
-  {
-    collections: ['Collection:SD-terms', 'Collection:Dáhtábágo javllamáno 2013'],
-    definition: null,
-    explanation: null,
-    terms: [
-      {
-        expression: {
-          language: 'sma',
-          lemma: 'fijle',
-          pos: 'N',
-        },
-        note: null,
-        source: null,
-        status: null,
-      },
-    ],
-  },
-];
+const lemma = 'vuorká';
+const termsByNames = multilingualconceptListsByNames(conceptList);
 
 export default {
   title: 'TermWikiArticle',
@@ -160,9 +13,9 @@ export default {
 
 export const Default = () => (
   <TermWikiArticle
-    category="Dihtorteknologiija ja diehtoteknihkka"
-    multiLingualConcept={concepts}
-    lemma="vuorká"
+    category='Servodatdieđa'
+    multiLingualConcept={orderedMultilingualConcept(lemma, termsByNames['Servodatdieđa:vurkkohat'])}
+    lemma={lemma}
   />
 );
 
