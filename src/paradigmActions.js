13f13314b5188dd05cb2964d11b59d442e14d9e9
Børre Gaup <albbas@gmail.com>
2021-01-07 18:56:32 +0100

    Use the same setup for Paradigms as for Articles

--- src/paradigmActions.js
+++ src/paradigmActions.js
@@ -1,27 +1,59 @@
 import fetch from 'cross-fetch';
-import {handleErrors, toJson} from './utils';
+import {handleErrors} from './utils';
 
-export const REQUEST_PARADIGM = 'REQUEST_PARADIGM';
-export const RECEIVE_PARADIGM = 'RECEIVE_PARADIGM';
+export const FETCH_PARADIGM_BEGIN = 'FETCH_PARADIGM_BEGIN';
+export const FETCH_PARADIGM_SUCCESS = 'FETCH_PARADIGM_SUCCESS';
+export const FETCH_PARADIGM_FAILURE = 'FETCH_PARADIGM_FAILURE';
 
-export const requestParadigm = (stem) => ({
-  type: FETCH_PARADIGM_REQUEST,
-  stem
+export const fetchParadigmBegin = (stem) => ({
+  type: FETCH_PARADIGM_BEGIN,
+  payload: {
+    stem
+  }
 });
 
-export const receiveParadigm = (stem, text) => ({
-  type: RECEIVE_PARADIGM,
-  stem,
-  paradigm: json
+export const fetchParadigmSuccess = (stem, json) => ({
+  type: FETCH_PARADIGM_SUCCESS,
+  payload: {
+    stem,
+    paradigm: json
+  }
+});
+
+export const fetchParadigmFailure = (stem, error) => ({
+  type: FETCH_PARADIGM_FAILURE,
+  payload: {
+    stem,
+    error
+  }
 });
 
 export const fetchParadigm = (stem) => (dispatch) => {
-  dispatch(requestParadigm(stem));
+  dispatch(fetchParadigmBegin(stem));
 
   let url = `http://gtweb.uit.no/cgi-bin/smi/smi.cgi?json=true&text=${stem.lemma}&pos=${stem.pos}&mode=standard&action=paradigm&lang=${stem.lang}`;
-  console.log(encodeURI(url));
+
   return fetch(encodeURI(url, {credentials: 'same-origin', mode: 'no-cors'}))
       .then(handleErrors)
-      .then(response => response.text())
-      .then(text => dispatch(receiveParadigm(stem, text)));
+      .then(response => response.json())
+      .then(json => dispatch(fetchParadigmSuccess(stem, json)))
+      .catch(error => (
+        dispatch(fetchParadigmFailure(stem, error))
+      )
+    );
+};
+
+export const shouldFetchParadigm = (state, key) => {
+  const paradigm = state.paradigm;
+  if (paradigm.has(key)) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+export const fetchParadigmIfNeeded = (stem) => (dispatch, getState) => {
+  if (shouldFetchParadigm(getState(), `${stem.lemma}_${stem.pos}_${stem.lang}`)) {
+    return dispatch(fetchParadigm(stem));
+  }
 };
