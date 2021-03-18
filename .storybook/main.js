db9667d08d31a893e10dd1355366040b7affd09d
Børre Gaup <albbas@gmail.com>
2021-03-18 11:49:44 +0100

    New version of storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,11 @@
 module.exports = {
-  stories: ['../src/**/*.stories.js'],
-  addons: [
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-    '@storybook/addon-viewport',
-    'storybook-addon-linguijs',
-    '@storybook/preset-create-react-app'
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-};
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app"
+  ]
+}
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,23 +1,4 @@
-import { addDecorator } from '@storybook/react';
-import { setLinguiConfig, withLingui } from 'storybook-addon-linguijs';
-import StoryWrapper from 'StoryWrapper';
-import catalogSe from '../src/locales/se/messages.js';
-import catalogNb from '../src/locales/nb/messages.js';
-import catalogEn from '../src/locales/en/messages.js';
 
-// Set configuration
-const catalogs = {
-  nb: catalogNb,
-  se: catalogSe,
-  en: catalogEn
-};
-
-setLinguiConfig({
-  locales: ['en', 'nb', 'se'],
-  defaultLocale: 'se',
-  catalogs
-});
-
-// Register decorator
-addDecorator(withLingui);
-addDecorator(StoryWrapper);
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+}
