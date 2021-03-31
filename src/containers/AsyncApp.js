f077194fd3d8a2c883dbe189b043a83f580a1966
Børre Gaup <albbas@gmail.com>
2021-03-31 16:38:19 +0200

    Prepare to get articles for more than one lemma

--- src/containers/AsyncApp.js
+++ src/containers/AsyncApp.js
@@ -14,7 +14,7 @@ import PropTypes from 'prop-types';
 import qs from 'qs';
 
 import { locationParser } from 'utils';
-import Articles from 'features/article/Articles';
+import ArticlesGroup from 'features/article/ArticlesGroup';
 import { WelcomeHeader } from 'containers/Welcome';
 import InfiniteStems from 'features/infinitestems/InfiniteStems';
 import SatniAppBar from './SatniAppBar';
@@ -137,8 +137,8 @@ const AsyncApp = ({classes}) => {
               </Grid>
               <Grid item xs={8}>
                 {currentLemma &&
-                  <Articles
-                    lemma={currentLemma}
+                  <ArticlesGroup
+                    lemmas={[currentLemma]}
                     wantedDicts={wantedDicts}
                     wantedLangs={data.wantedLangs}
                   />
