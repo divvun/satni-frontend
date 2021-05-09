9fa4d6775cf5e0f33a1ba68be0d5d2c5405f33ab
Børre Gaup <albbas@gmail.com>
2021-05-09 13:12:01 +0200

    "Wildcard" search

---
+++ src/features/search/WildCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+
+export default function WildCard() {
+  const [value, setValue] = React.useState('start');
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <FormControl component="fieldset">
+      <RadioGroup row aria-label="searchFrom" name="searchFrom1" value={value} onChange={handleChange}>
+        <FormControlLabel value="start" control={<Radio />} label="Start" />
+        <FormControlLabel value="middle" control={<Radio />} label="Middle" />
+        <FormControlLabel value="end" control={<Radio />} label="End" />
+      </RadioGroup>
+    </FormControl>
+  );
+}
---
+++ src/features/search/WildCard.stories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import WildCard from './WildCard';
+
+export default {
+  title: 'WildCard',
+};
+
+export const Default = () => (
+  <WildCard />
+);
+
+Default.story = {
+  name: 'default',
+};
