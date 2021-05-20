194ed91127485a3f6e2c1dcb5273892d03aa6719
Børre Gaup <albbas@gmail.com>
2021-05-20 21:46:41 +0200

    Move langs and dicts into the infinitestems hook

--- src/features/infinitestems/InfiniteStems.hooks.js
+++ src/features/infinitestems/InfiniteStems.hooks.js
@@ -1,10 +1,23 @@
 import { useQuery } from '@apollo/client';
+import { useLocation } from 'react-router-dom';
+import { locationParser } from '../../utils';
 import GET_LEMMAS from '../../operations/queries/getLemmas';
 import GET_SEARCH_MODE from '../../operations/queries/getSearchMode';
+import GET_LANGS_DICTS from '../../operations/queries/getLangsDicts';
 
-function useStems(inputValue, wantedDicts, wantedLangs) {
+function useStems(inputValue) {
   const searchModeQueryResult = useQuery(GET_SEARCH_MODE);
   const { searchMode } = searchModeQueryResult.data;
+  const langsDictsQueryResult = useQuery(GET_LANGS_DICTS);
+  const { wantedLangs } = langsDictsQueryResult.data;
+
+  const location = useLocation();
+  const { currentDict } = locationParser(location.pathname);
+
+  const wantedDicts = currentDict
+    ? [currentDict]
+    : langsDictsQueryResult.data.wantedDicts;
+
   const { data, loading, fetchMore, error } = useQuery(GET_LEMMAS, {
     notifyOnNetworkStatusChange: true,
     variables: {
