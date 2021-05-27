fd5054359480f6e1c50b895b5b4769835774db35
Børre Gaup <albbas@gmail.com>
2021-05-27 22:08:10 +0200

    Activate Sentry, it was disabled by an accident

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-// import * as Sentry from '@sentry/browser';
+import * as Sentry from '@sentry/react';
 import React from 'react';
 import { render } from 'react-dom';
 import Root from './containers/Root';
 import satniStore from './configureStore';
 import apolloClient from './apolloClient';
 
-// Sentry.init({
-//   dsn: 'https://65960ebe11524e22b83637dfe43344a2@sentry.io/1303235',
-// });
+Sentry.init({
+  dsn: 'https://65960ebe11524e22b83637dfe43344a2@sentry.io/1303235',
+});
 
 render(
   <Root store={satniStore} client={apolloClient} />,
