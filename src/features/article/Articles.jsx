43a808896e26dc14667bace48ac4492c3eb06971
Børre Gaup <albbas@gmail.com>
2021-05-27 21:51:15 +0200

    Add a «statusinfo» above the article list, as well

--- src/features/article/Articles.jsx
+++ src/features/article/Articles.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Trans } from '@lingui/macro';
+import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import DictArticles from './DictArticles';
@@ -11,15 +13,23 @@ const useStyles = makeStyles((theme) => ({
     paddingRight: theme.spacing(2),
     paddingLeft: theme.spacing(2),
   },
+  status: {
+    textAlign: 'center',
+  },
 }));
 
 const Articles = ({ lemma }) => {
   const classes = useStyles();
 
   return (
-    <div className={classes.articles}>
-      <DictArticles lemma={lemma} />
-      <TermArticles lemma={lemma} />
+    <div>
+      <Typography className={classes.status}>
+        <Trans>Results for «{lemma}»</Trans>
+      </Typography>
+      <div className={classes.articles}>
+        <DictArticles lemma={lemma} />
+        <TermArticles lemma={lemma} />
+      </div>
     </div>
   );
 };
