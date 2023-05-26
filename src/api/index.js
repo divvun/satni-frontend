d1557b024828d367799ff6dac2543ce45afa3a10
Børre Gaup <albbas@gmail.com>
2023-05-26 14:47:56 +0200

    Avoid redirect when calling API
    These little details led to CORS error, which made corpus icons
    inactive.
    
    Fixes #68

--- src/api/index.js
+++ src/api/index.js
@@ -2,27 +2,27 @@ import fetch from 'cross-fetch';
 
 const korpInfo = {
   sma: {
-    start_query: 'http://gtweb.uit.no/ws/gt_korp?command=query&corpus=',
+    start_query: 'http://gtweb.uit.no/ws/gt_korp/?command=query&corpus=',
     corpora:
       'SMA_ADMIN_20181106,SMA_BIBLE_20181106,SMA_FACTA_20181106,SMA_FICTI_20181106,SMA_LAWS_20181106,SMA_NEWS_20181106,SMA_SCIENCE_20181106',
   },
   sme: {
-    start_query: 'http://gtweb.uit.no/ws/gt_korp?command=query&corpus=',
+    start_query: 'http://gtweb.uit.no/ws/gt_korp/?command=query&corpus=',
     corpora:
       'SME_ADMIN_20181106,SME_ASSU_20181106,SME_AVVIR_20181106,SME_BIBLE_20181106,SME_BLOGS_20181106,SME_FACTA_20181106,SME_FICTI_20181106,SME_LAWS_20181106,SME_MINAIGI_20181106,SME_MUITALUS_20170319,SME_NRK_20181106,SME_SCIENCE_20181106',
   },
   smj: {
-    start_query: 'http://gtweb.uit.no/ws/gt_korp?command=query&corpus=',
+    start_query: 'http://gtweb.uit.no/ws/gt_korp/?command=query&corpus=',
     corpora:
       'SMJ_ADMIN_20181106%2CSMJ_BIBLE_20181106%2CSMJ_FACTA_20181106%2CSMJ_FICTI_20181106%2CSMJ_LAWS_20181106%2CSMJ_NEWS_20181106%2CSMJ_SCIENCE_20181106',
   },
   smn: {
-    start_query: 'http://gtweb.uit.no/ws/gt_korp?command=query&corpus=',
+    start_query: 'http://gtweb.uit.no/ws/gt_korp/?command=query&corpus=',
     corpora:
       'SMN_ADMIN_20181106,SMN_BIBLE_20181106,SMN_FACTA_20181106,SMN_NEWS_20181106,SMN_SCIENCE_20181106',
   },
   sms: {
-    start_query: 'http://gtweb.uit.no/ws/gt_korp?command=query&corpus=',
+    start_query: 'http://gtweb.uit.no/ws/gt_korp/?command=query&corpus=',
     corpora:
       'SMS_ADMIN_20181106,SMS_FACTA_20181106,SMS_FICTI_20181106,SMS_LAWS_20181106,SMS_LITERATURE_20171108,SMS_SCIENCE_20181106',
   },
