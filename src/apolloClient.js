fb86a53690855ce89e999f858591f1d3d234e36c
Børre Gaup <albbas@gmail.com>
2023-09-07 23:24:58 +0200

    No need for the restlink anymore, all paradigm queries are done on satni-backend

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,17 +1,12 @@
 import { ApolloClient, ApolloLink, HttpLink } from '@apollo/client';
-import { RestLink } from 'apollo-link-rest';
 import { cache } from './apolloCache';
-
-const paradigmLink = new RestLink({
-  uri: 'http://gtweb.uit.no/cgi-bin/smi/',
-});
 
 const SATNI_ENDPOINT =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:8000/graphql/'
     : 'https://satni.uit.no/newsatni/';
 const dictLink = new HttpLink({ uri: SATNI_ENDPOINT });
-const link = ApolloLink.from([paradigmLink, dictLink]);
+const link = ApolloLink.from([dictLink]);
 
 const apolloClient = new ApolloClient({
   cache,
