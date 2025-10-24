a256ec841bf3fee347761bbd02be65852c8ba633
Børre Gaup <albbas@gmail.com>
2025-10-24 12:14:32 +0200

    storybook runs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,22 @@
-module.exports = {
+export default {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials'
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: {
-    name: '@storybook/react-webpack5',
-    options: {}
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  typescript: {
+    check: false,
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
+  },
+  viteFinal: async (config) => {
+    config.esbuild = config.esbuild || {};
+    config.esbuild.loader = config.esbuild.loader || {};
+    config.esbuild.loader['.js'] = 'jsx';
+    return config;
   },
 };
