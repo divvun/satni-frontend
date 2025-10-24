03e98cdbbfbf5afaae57fa5c772fa8ea281b2b7a
Børre Gaup <albbas@gmail.com>
2025-10-24 20:30:10 +0200

    Cache api calls to korp

--- src/features/korp/korpSlice.test.ts
+++ src/features/korp/korpSlice.test.ts
@@ -2,6 +2,7 @@ import korp, {
   getKorpFailure,
   getKorpStart,
   getKorpSuccess,
+  getKorpFromCache,
 } from './korpSlice';
 
 describe('reducers', () => {
@@ -9,6 +10,7 @@ describe('reducers', () => {
     expect(korp(undefined, {} as any)).toEqual({
       isFetching: false,
       error: null,
+      cache: {},
     });
   });
 
@@ -20,6 +22,7 @@ describe('reducers', () => {
     ).toEqual({
       error: null,
       isFetching: true,
+      cache: {},
     });
   });
 
@@ -32,6 +35,7 @@ describe('reducers', () => {
     ).toEqual({
       isFetching: false,
       error: 'Error',
+      cache: {},
     });
   });
 
@@ -41,12 +45,58 @@ describe('reducers', () => {
         type: getKorpSuccess.type,
         payload: {
           korp: true,
+          cacheKey: 'sme:muorra',
         },
       } as any),
     ).toEqual({
       isFetching: false,
       error: null,
       lemmaExists: true,
+      cache: {
+        'sme:muorra': true,
+      },
+    });
+  });
+
+  it('should handle FETCH_KORP_FROM_CACHE', () => {
+    expect(
+      korp(undefined, {
+        type: getKorpFromCache.type,
+        payload: true,
+      } as any),
+    ).toEqual({
+      isFetching: false,
+      error: null,
+      lemmaExists: true,
+      cache: {},
+    });
+  });
+
+  it('should cache multiple lemma results', () => {
+    const state1 = korp(undefined, {
+      type: getKorpSuccess.type,
+      payload: {
+        korp: true,
+        cacheKey: 'sme:muorra',
+      },
+    } as any);
+
+    const state2 = korp(state1, {
+      type: getKorpSuccess.type,
+      payload: {
+        korp: false,
+        cacheKey: 'sma:tjidtjie',
+      },
+    } as any);
+
+    expect(state2).toEqual({
+      isFetching: false,
+      error: null,
+      lemmaExists: false,
+      cache: {
+        'sme:muorra': true,
+        'sma:tjidtjie': false,
+      },
     });
   });
 });
