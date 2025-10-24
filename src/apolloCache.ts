89404cd7046dfc26924c10b11063a6c09a621ea0
Børre Gaup <albbas@gmail.com>
2025-10-24 13:07:14 +0200

    api layer files converted to ts

--- src/apolloCache.ts
+++ src/apolloCache.ts
@@ -1,20 +1,17 @@
-import { InMemoryCache, makeVar } from '@apollo/client';
+import { InMemoryCache, makeVar, ReactiveVar } from '@apollo/client';
 import { relayStylePagination } from '@apollo/client/utilities';
 import dictionaryInfo from './translateble_variables';
 import { availableLanguages } from './utils';
 
-export const availableDictsVar = makeVar(Object.keys(dictionaryInfo));
+export const availableDictsVar: ReactiveVar<string[]> = makeVar(Object.keys(dictionaryInfo));
 
-export const interfaceLanguageVar = makeVar(
-  localStorage.getItem('interfaceLanguage')
-    ? localStorage.getItem('interfaceLanguage')
-    : 'se',
+export const interfaceLanguageVar: ReactiveVar<string> = makeVar(
+  localStorage.getItem('interfaceLanguage') || 'se',
 );
 
-const getWanted = () => {
-  const unwanted = localStorage.getItem('unwantedDicts')
-    ? JSON.parse(localStorage.getItem('unwantedDicts'))
-    : [];
+const getWanted = (): string[] => {
+  const unwantedDictsItem = localStorage.getItem('unwantedDicts');
+  const unwanted = unwantedDictsItem ? JSON.parse(unwantedDictsItem) : [];
 
   return unwanted
     ? Object.keys(dictionaryInfo).filter(
@@ -23,23 +20,23 @@ const getWanted = () => {
     : Object.keys(dictionaryInfo);
 };
 
-export const wantedDictsVar = makeVar(getWanted());
+export const wantedDictsVar: ReactiveVar<string[]> = makeVar(getWanted());
 
-export const srcLangsVar = makeVar(
+export const srcLangsVar: ReactiveVar<string[]> = makeVar(
   localStorage.getItem('srcLangs')
-    ? JSON.parse(localStorage.getItem('srcLangs'))
+    ? JSON.parse(localStorage.getItem('srcLangs')!)
     : availableLanguages,
 );
 
-export const targetLangsVar = makeVar(
+export const targetLangsVar: ReactiveVar<string[]> = makeVar(
   localStorage.getItem('targetLangs')
-    ? JSON.parse(localStorage.getItem('targetLangs'))
+    ? JSON.parse(localStorage.getItem('targetLangs')!)
     : availableLanguages,
 );
 
-export const searchModeVar = makeVar('start');
+export const searchModeVar: ReactiveVar<string> = makeVar('start');
 
-export const searchListClickedItemVar = makeVar(-1);
+export const searchListClickedItemVar: ReactiveVar<number> = makeVar(-1);
 
 export const cache = new InMemoryCache({
   typePolicies: {
--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,10 +1,11 @@
 import { ApolloClient, ApolloLink, HttpLink } from '@apollo/client';
 import { cache } from './apolloCache';
 
-const SATNI_ENDPOINT =
+const SATNI_ENDPOINT: string =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:8000/graphql/'
     : 'https://satni.uit.no/newsatni/';
+
 const dictLink = new HttpLink({ uri: SATNI_ENDPOINT });
 const link = ApolloLink.from([dictLink]);
 
