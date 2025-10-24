56fdc12992168ea16ee6d9efd03264eafc5ba026
Børre Gaup <albbas@gmail.com>
2025-10-24 15:06:13 +0200

    Upgrade to react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import * as Sentry from '@sentry/react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import apolloClient from './apolloClient';
 import satniStore from './configureStore';
 import Root from './containers/Root';
@@ -8,8 +8,8 @@ Sentry.init({
   dsn: 'https://65960ebe11524e22b83637dfe43344a2@sentry.io/1303235',
 });
 
-// @ts-expect-error - React 17 API with React 19 types mismatch
-ReactDOM.render(
-  <Root store={satniStore} client={apolloClient} />,
-  document.getElementById('root'),
-);
+const container = document.getElementById('root');
+if (!container) throw new Error('Failed to find the root element');
+
+const root = createRoot(container);
+root.render(<Root store={satniStore} client={apolloClient} />);
