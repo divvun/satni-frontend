78470dddef8490eba5a8680f1c41358f222a88c8
Børre Gaup <albbas@gmail.com>
2025-11-03 21:37:50 +0100

    Make satni.org useful on mobile phones

--- src/containers/SatniAppBar.tsx
+++ src/containers/SatniAppBar.tsx
@@ -4,12 +4,19 @@ import { Trans } from "@lingui/react/macro";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
 import Toolbar from "@mui/material/Toolbar";
 import Tooltip from "@mui/material/Tooltip";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
 // import Typography from "@mui/material/Typography";
 import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 import MenuIcon from "@mui/icons-material/Menu";
 import HomeIcon from "@mui/icons-material/Home";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { useState } from "react";
 // import { Link } from "react-router-dom";
 
@@ -29,12 +36,34 @@ const SatniAppBar: React.FC<SatniAppBarProps> = ({
   handleDrawerToggle,
   drawerWidth,
 }) => {
+  const theme = useTheme();
+  const isVeryNarrow = useMediaQuery(theme.breakpoints.down(400));
   const [openHelpDialog, setOpenHelpDialog] = useState<boolean>(false);
+  const [moreMenuAnchor, setMoreMenuAnchor] = useState<null | HTMLElement>(null);
+  
   const handleClickHelpDialog = () => {
     setOpenHelpDialog(true);
   };
   const handleCloseHelpDialog = () => {
     setOpenHelpDialog(false);
+  };
+  
+  const handleMoreMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setMoreMenuAnchor(event.currentTarget);
+  };
+  
+  const handleMoreMenuClose = () => {
+    setMoreMenuAnchor(null);
+  };
+  
+  const handleDrawerClick = () => {
+    handleMoreMenuClose();
+    handleDrawerToggle();
+  };
+  
+  const handleHelpClick = () => {
+    handleMoreMenuClose();
+    handleClickHelpDialog();
   };
 
   return (
@@ -49,50 +78,104 @@ const SatniAppBar: React.FC<SatniAppBarProps> = ({
       }}
     >
       <Toolbar variant="dense" sx={{ gap: 1 }}>
-        <Box sx={{ display: { xs: "flex", md: "none" }, alignItems: "center" }}>
-          <Tooltip title={<Trans>Menu</Trans>}>
-            <IconButton
-              color="inherit"
-              aria-label="Open drawer"
-              edge="start"
-              onClick={handleDrawerToggle}
-              sx={{ mr: 2 }}
+        {isVeryNarrow ? (
+          // Very narrow devices: Only search field + menu button
+          <>
+            <Box sx={{ flexGrow: 1, display: "flex", alignItems: "center" }}>
+              <FilterBar
+                inAppBar
+                searchExpression={searchExpression}
+                setSearchExpression={setSearchExpression}
+              />
+            </Box>
+            <Tooltip title={<Trans>More</Trans>}>
+              <IconButton
+                color="inherit"
+                aria-label="More options"
+                edge="end"
+                onClick={handleMoreMenuOpen}
+              >
+                <MoreVertIcon />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              anchorEl={moreMenuAnchor}
+              open={Boolean(moreMenuAnchor)}
+              onClose={handleMoreMenuClose}
             >
-              <MenuIcon />
-            </IconButton>
-          </Tooltip>
-          {/* Mobile-only home icon linking to sátni.org */}
-          <Tooltip title={<Trans>Home</Trans>}>
-            <IconButton
-              color="inherit"
-              aria-label="Home"
-              edge="start"
-              component="a"
-              href="https://satni.org"
-              sx={{ mr: 1 }}
-            >
-              <HomeIcon />
-            </IconButton>
-          </Tooltip>
-        </Box>
-        {/* Search bar inside the AppBar */}
-        <Box sx={{ flexGrow: 1, display: "flex", alignItems: "center" }}>
-          <FilterBar
-            inAppBar
-            searchExpression={searchExpression}
-            setSearchExpression={setSearchExpression}
-          />
-        </Box>
-        <Tooltip title={<Trans>Help</Trans>}>
-          <IconButton
-            color="inherit"
-            aria-label="Help"
-            edge="end"
-            onClick={handleClickHelpDialog}
-          >
-            <HelpOutlineIcon />
-          </IconButton>
-        </Tooltip>
+              <MenuItem onClick={handleDrawerClick}>
+                <ListItemIcon>
+                  <MenuIcon fontSize="small" />
+                </ListItemIcon>
+                <ListItemText>
+                  <Trans>Menu</Trans>
+                </ListItemText>
+              </MenuItem>
+              <MenuItem component="a" href="https://satni.org" onClick={handleMoreMenuClose}>
+                <ListItemIcon>
+                  <HomeIcon fontSize="small" />
+                </ListItemIcon>
+                <ListItemText>
+                  <Trans>Home</Trans>
+                </ListItemText>
+              </MenuItem>
+              <MenuItem onClick={handleHelpClick}>
+                <ListItemIcon>
+                  <HelpOutlineIcon fontSize="small" />
+                </ListItemIcon>
+                <ListItemText>
+                  <Trans>Help</Trans>
+                </ListItemText>
+              </MenuItem>
+            </Menu>
+          </>
+        ) : (
+          // Normal mobile/desktop: Show all controls
+          <>
+            <Box sx={{ display: { xs: "flex", md: "none" }, alignItems: "center" }}>
+              <Tooltip title={<Trans>Menu</Trans>}>
+                <IconButton
+                  color="inherit"
+                  aria-label="Open drawer"
+                  edge="start"
+                  onClick={handleDrawerToggle}
+                  sx={{ mr: 2 }}
+                >
+                  <MenuIcon />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title={<Trans>Home</Trans>}>
+                <IconButton
+                  color="inherit"
+                  aria-label="Home"
+                  edge="start"
+                  component="a"
+                  href="https://satni.org"
+                  sx={{ mr: 1 }}
+                >
+                  <HomeIcon />
+                </IconButton>
+              </Tooltip>
+            </Box>
+            <Box sx={{ flexGrow: 1, display: "flex", alignItems: "center" }}>
+              <FilterBar
+                inAppBar
+                searchExpression={searchExpression}
+                setSearchExpression={setSearchExpression}
+              />
+            </Box>
+            <Tooltip title={<Trans>Help</Trans>}>
+              <IconButton
+                color="inherit"
+                aria-label="Help"
+                edge="end"
+                onClick={handleClickHelpDialog}
+              >
+                <HelpOutlineIcon />
+              </IconButton>
+            </Tooltip>
+          </>
+        )}
       </Toolbar>
       <HelpDialog open={openHelpDialog} onClose={handleCloseHelpDialog} />
     </AppBar>
